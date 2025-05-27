28e57188209f529821601d6eea989e42e49b4516
shsaish006 <shsaish006@gmail.com>
2025-05-27 13:58:32 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (75.00%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,17 @@
+//SHIVAM#include <vector>
+
+class Solution {
+public:
+  int removeElement(std::vector<int>& nums, int val) {
+    int j = 0;  
+
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] != val) {
+        nums[j] = nums[i];  
+        j++;
+      }
+    }
+
+    return j;  
+  }
+};
