39aebe1ce6e267d3b0e770d23305851fbab3a001
shsaish006 <shsaish006@gmail.com>
2025-05-27 23:25:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (96.48%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string stk;
+        for(char c:s){
+            if(isdigit(c)){
+                stk.pop_back();
+            }else{
+                stk.push_back(c);
+            }
+        }
+        return stk;
+        
+    }
+};
