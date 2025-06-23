3d68e80a1b87d4e0e4f43a906f43183ab83ff93b
shsaish006 <shsaish006@gmail.com>
2025-06-23 12:14:04 +0530

    Time: 3035 ms (5.26%) | Memory: 558.9 MB (5.26%) - LeetSync

--- 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.cpp
+++ 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.cpp
@@ -2,38 +2,50 @@ class Solution {
 public:
     long long kMirror(int k, int n) {
         long long ans = 0;
-        for (int l = 1;; ++l) {
-            int x = pow(10, (l - 1) / 2);
-            int y = pow(10, (l + 1) / 2);
-            for (int i = x; i < y; ++i) {
-                long long v = i;
-                int j = (l % 2 == 0) ? i : i / 10;
-                while (j > 0) {
-                    v = v * 10 + j % 10;
-                    j /= 10;
-                }
-                if (check(v, k)) {
-                    ans += v;
-                    if (--n == 0) {
+        int count = 0;
+
+        for (int len = 1; count < n; ++len) {
+            vector<long long> pals = generatePalindromes(len);
+            for (long long num : pals) {
+                if (isKPalindrome(num, k)) {
+                    ans += num;
+                    count++;
+                    if (count == n)
                         return ans;
-                    }
                 }
             }
         }
+
+        return ans;
     }
 
 private:
-    bool check(long long x, int k) {
-        vector<int> s;
+    vector<long long> generatePalindromes(int len) {
+        vector<long long> res;
+        int half = (len + 1) / 2;
+        int start = pow(10, half - 1);
+        int end = pow(10, half);
+
+        for (int i = start; i < end; ++i) {
+            string first = to_string(i);
+            string second = first;
+            if (len % 2 == 1)
+                second.pop_back();
+            reverse(second.begin(), second.end());
+            res.push_back(stoll(first + second));
+        }
+        return res;
+    }
+
+    bool isKPalindrome(long long x, int k) {
+        vector<int> digits;
         while (x > 0) {
-            s.push_back(x % k);
+            digits.push_back(x % k);
             x /= k;
         }
-        for (int i = 0, j = s.size() - 1; i < j; ++i, --j) {
-            if (s[i] != s[j]) {
+        for (int i = 0, j = digits.size() - 1; i < j; ++i, --j)
+            if (digits[i] != digits[j])
                 return false;
-            }
-        }
         return true;
     }
 };
