383467dbce00d1ac7746807ef46548ccc50f0a7c
shsaish006 <shsaish006@gmail.com>
2025-06-29 13:06:16 +0530

    Time: 29 ms (71.12%) | Memory: 54.3 MB (34.63%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,21 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int numSubseq(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        const int mod = 1e9 + 7;
+        const int MOD = 1e9 + 7;
         int n = nums.size();
-        int f[n + 1];
-        f[0] = 1;
-        for (int i = 1; i <= n; ++i) {
-            f[i] = (f[i - 1] * 2) % mod;
-        }
-        int ans = 0;
-        for (int i = 0; i < n && nums[i] * 2 <= target; ++i) {
-            int j = upper_bound(nums.begin() + i + 1, nums.end(), target - nums[i]) - nums.begin() - 1;
-            ans = (ans + f[j - i]) % mod;
-        }
-        return ans;
 
-        
+        sort(nums.begin(), nums.end());
+
+        vector<int> powerOfTwo(n + 1, 1);
+        for (int i = 1; i <= n; ++i) {
+            powerOfTwo[i] = (powerOfTwo[i - 1] * 2) % MOD;
+        }
+
+        int left = 0, right = n - 1;
+        int result = 0;
+
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                result = (result + powerOfTwo[right - left]) % MOD;
+                ++left;
+            } else {
+                --right;
+            }
+        }
+
+        return result;
     }
 };
