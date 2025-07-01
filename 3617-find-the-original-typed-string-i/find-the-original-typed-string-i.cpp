d3847d3dcfe9ba177aebb9f10d2a7204eae6949c
shsaish006 <shsaish006@gmail.com>
2025-07-01 11:59:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (96.36%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -1,9 +1,28 @@
+// class Solution {
+// public:
+//     int possibleStringCount(string word) {
+//         int f = 0;
+//         int i = 0;
+//         while (i < word.size()) {
+//             char current = word[i];
+//             f++;
+//             while (i < word.size() && word[i] == current) {
+//                 i++;
+//             }
+//         }
+//         return f;
+//     }
+// };
+
+
 class Solution {
 public:
     int possibleStringCount(string word) {
         int f = 1;
         for (int i = 1; i < word.size(); ++i) {
-            f += word[i] == word[i - 1];
+            if (word[i] == word[i - 1]) {
+                f++;
+            }
         }
         return f;
     }
