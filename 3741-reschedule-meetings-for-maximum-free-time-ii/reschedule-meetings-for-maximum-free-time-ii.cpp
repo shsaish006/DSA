1b19cf7c8a5dcf3108995d8ec0a9e47a9d23f733
shsaish006 <shsaish006@gmail.com>
2025-07-10 18:55:54 +0530

    Time: 4 ms (94.42%) | Memory: 144.7 MB (83.67%) - LeetSync

--- 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -4,35 +4,42 @@ using namespace std;
 
 class Solution {
 public:
-    int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
+    int maxFreeTime(int eventTime, const vector<int>& startTime, const vector<int>& endTime) {
         int n = startTime.size();
-        int res = 0;
+        int maxTime = 0;
 
-        vector<int> left_gaps(n, 0);
-        left_gaps[0] = startTime[0];
-        for (int meet = 1; meet < n; ++meet) {
-            left_gaps[meet] = max(left_gaps[meet - 1], startTime[meet] - endTime[meet - 1]);
+        vector<int> leftGaps(n);
+        leftGaps[0] = startTime[0];
+        for (int i = 1; i < n; ++i) {
+            int gap = startTime[i] - endTime[i - 1];
+            leftGaps[i] = max(leftGaps[i - 1], gap);
         }
 
-        vector<int> right_gaps(n, 0);
-        right_gaps[n - 1] = eventTime - endTime[n - 1];
-        for (int meet = n - 2; meet >= 0; --meet) {
-            right_gaps[meet] = max(right_gaps[meet + 1], startTime[meet + 1] - endTime[meet]);
+        vector<int> rightGaps(n);
+        rightGaps[n - 1] = eventTime - endTime[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            int gap = startTime[i + 1] - endTime[i];
+            rightGaps[i] = max(rightGaps[i + 1], gap);
         }
 
-        for (int meet = 0; meet < n; ++meet) {
-            int left_gap = (meet == 0) ? left_gaps[meet] : startTime[meet] - endTime[meet - 1];
-            int right_gap = (meet == n - 1) ? right_gaps[meet] : startTime[meet + 1] - endTime[meet];
+        for (int i = 0; i < n; ++i) {
+            int leftGap = (i == 0) ? leftGaps[i] : startTime[i] - endTime[i - 1];
+            int rightGap = (i == n - 1) ? rightGaps[i] : startTime[i + 1] - endTime[i];
+            int meetingDuration = endTime[i] - startTime[i];
 
-            int interval = 0;
-            if ((meet != 0 && left_gaps[meet - 1] >= (endTime[meet] - startTime[meet])) ||
-                (meet != n - 1 && right_gaps[meet + 1] >= (endTime[meet] - startTime[meet]))) {
-                interval = endTime[meet] - startTime[meet];
+            int includeMeeting = 0;
+            if ((i > 0 && leftGaps[i - 1] >= meetingDuration) ||
+                (i < n - 1 && rightGaps[i + 1] >= meetingDuration)) {
+                includeMeeting = meetingDuration;
             }
 
-            res = max(res, left_gap + interval + right_gap);
+            int total = leftGap + includeMeeting + rightGap;
+            maxTime = max(maxTime, total);
         }
 
-        return res;
+        return maxTime;
     }
+
+
+    
 };
