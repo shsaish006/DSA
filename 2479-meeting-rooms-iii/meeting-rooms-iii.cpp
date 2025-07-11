6d833d439e17ee01d380a29203848fc13abab7b8
shsaish006 <shsaish006@gmail.com>
2025-07-11 07:04:43 +0530

    Time: 64 ms (90.38%) | Memory: 98 MB (91.68%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -4,38 +4,37 @@ using pii = pair<ll, int>;
 class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
-        priority_queue<int, vector<int>, greater<int>> idle;
-        priority_queue<pii, vector<pii>, greater<pii>> busy;
-        for (int i = 0; i < n; ++i) idle.push(i);
-        vector<int> cnt(n);
-        sort(meetings.begin(), meetings.end());
-        for (auto& v : meetings) {
-            int s = v[0], e = v[1];
-            while (!busy.empty() && busy.top().first <= s) {
-                idle.push(busy.top().second);
-                busy.pop();
-            }
-            int i = 0;
-            if (!idle.empty()) {
-                i = idle.top();
-                idle.pop();
-                busy.push({e, i});
-            } else {
-                auto x = busy.top();
-                busy.pop();
-                i = x.second;
-                busy.push({x.first + e - s, i});
-            }
-            ++cnt[i];
-        }
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            if (cnt[ans] < cnt[i]) {
-                ans = i;
-            }
-        }
-        return ans;
-    }
+        vector<int> count(n);
+        priority_queue<int, vector<int>, greater<>> available;
+        priority_queue<pii, vector<pii>, greater<>> occupied;
 
-    
+        for (int i = 0; i < n; ++i) available.push(i);
+        sort(meetings.begin(), meetings.end());
+
+        for (auto& m : meetings) {
+            ll start = m[0], end = m[1];
+            while (!occupied.empty() && occupied.top().first <= start) {
+                available.push(occupied.top().second);
+                occupied.pop();
+            }
+            if (!available.empty()) {
+                int room = available.top();
+                available.pop();
+                occupied.push({end, room});
+                count[room]++;
+            } else {
+                auto [freeTime, room] = occupied.top();
+                occupied.pop();
+                ll newEnd = freeTime + (end - start);
+                occupied.push({newEnd, room});
+                count[room]++;
+            }
+        }
+
+        int res = 0;
+        for (int i = 1; i < n; ++i) {
+            if (count[i] > count[res]) res = i;
+        }
+        return res;
+    }
 };
