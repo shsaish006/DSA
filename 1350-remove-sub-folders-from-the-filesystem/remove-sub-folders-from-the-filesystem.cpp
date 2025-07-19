c11e2af87a9b3b8a0e354b2e0c635bae78028b19
shsaish006 <shsaish006@gmail.com>
2025-07-19 12:21:59 +0530

    Time: 223 ms (15.05%) | Memory: 137.2 MB (11.61%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,15 +1,53 @@
+class TrieNode {
+public:
+    unordered_map<string, TrieNode*> children;
+    bool isEnd = false;
+};
+
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        sort(folder.begin(), folder.end());
-        vector<string> ans = {folder[0]};
-        for (int i = 1; i < folder.size(); ++i) {
-            int m = ans.back().size();
-            int n = folder[i].size();
-            if (m >= n || !(ans.back() == folder[i].substr(0, m) && folder[i][m] == '/')) {
-                ans.emplace_back(folder[i]);
+        TrieNode* root = new TrieNode();
+
+        auto split = [](const string& path) {
+            vector<string> parts;
+            stringstream ss(path);
+            string segment;
+            while (getline(ss, segment, '/')) {
+                if (!segment.empty())
+                    parts.push_back(segment);
             }
+            return parts;
+        };
+
+        for (const string& f : folder) {
+            TrieNode* node = root;
+            auto parts = split(f);
+            bool skip = false;
+            for (const string& part : parts) {
+                if (node->isEnd) {
+                    skip = true;
+                    break;
+                }
+                if (!node->children.count(part))
+                    node->children[part] = new TrieNode();
+                node = node->children[part];
+            }
+            if (!skip) node->isEnd = true;
         }
-        return ans;
+
+        vector<string> result;
+        function<void(TrieNode*, string)> dfs = [&](TrieNode* node, string path) {
+            if (node->isEnd) {
+                result.push_back(path);
+                return;
+            }
+            for (auto& [name, child] : node->children) {
+                dfs(child, path + "/" + name);
+            }
+        };
+
+        dfs(root, "");
+        return result;
     }
 };
