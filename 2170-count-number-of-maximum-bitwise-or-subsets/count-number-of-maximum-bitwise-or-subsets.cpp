ad38ccd20d6a3e9956fba6285d19b058ab8772af
shsaish006 <shsaish006@gmail.com>
2025-07-28 11:19:38 +0530

    Time: 8 ms (80.56%) | Memory: 10.6 MB (86.47%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    int countMaxOrSubsets(vector<int>& nums) {
-        int ans = 0;
-        int mx = accumulate(nums.begin(), nums.end(), 0, bit_or<int>());
-        auto dfs = [&](this auto&& dfs, int i, int t) {
-            if (i == nums.size()) {
-                if (t == mx) {
-                    ans++;
-                }
-                return;
+    int maxOr = 0;
+    int count = 0;
+
+    void dfs(const vector<int>& nums, int index, int currentOr) {
+        if (index == nums.size()) {
+            if (currentOr == maxOr) {
+                count++;
             }
-            dfs(i + 1, t);
-            dfs(i + 1, t | nums[i]);
-        };
-        dfs(0, 0);
-        return ans;
-        
+            return;
+        }
+
+        // Skip current number
+        dfs(nums, index + 1, currentOr);
+
+        // Include current number
+        dfs(nums, index + 1, currentOr | nums[index]);
+    }
+
+    int countMaxOrSubsets(vector<int>& nums) {
+        // Compute the maximum possible OR value
+        for (int num : nums) {
+            maxOr |= num;
+        }
+
+        dfs(nums, 0, 0);
+        return count;
     }
 };
