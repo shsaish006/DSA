993012f55480bb3abe6804d4916dd4ee6b045a1b
shsaish006 <shsaish006@gmail.com>
2025-07-29 12:30:00 +0530

    Time: 29 ms (56.90%) | Memory: 60.7 MB (71.12%) - LeetSync

--- 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    vector<int> smallestSubarrays(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> f(32, -1);
-        vector<int> ans(n);
-        for (int i = n - 1; ~i; --i) {
-            int t = 1;
-            for (int j = 0; j < 32; ++j) {
-                if ((nums[i] >> j) & 1) {
-                    f[j] = i;
-                } else if (f[j] != -1) {
-                    t = max(t, f[j] - i + 1);
+    vector<int> smallestSubarrays(vector<int>& a) {
+        int b = a.size();
+        vector<int> c(32, -1);  // Last seen positions of each bit
+        vector<int> d(b);       // Result array
+
+        for (int e = b - 1; ~e; --e) {
+            for (int f = 0; f < 32; ++f) {
+                if ((a[e] >> f) & 1) {
+                    c[f] = e;
                 }
             }
-            ans[i] = t;
+
+            int g = e;
+            for (int f = 0; f < 32; ++f) {
+                if (c[f] != -1) {
+                    g = max(g, c[f]);
+                }
+            }
+
+            d[e] = g - e + 1;
         }
-        return ans;
+
+        return d;
     }
 };
