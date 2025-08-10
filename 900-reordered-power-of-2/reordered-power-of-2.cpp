05517b783200ac2ff826f20bea730c2e4e9a9910
shsaish006 <shsaish006@gmail.com>
2025-08-10 11:49:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (100.00%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        string target = f(n);
-        for (int i = 1; i <= 1000000000; i <<= 1) {
-            if (target == f(i)) {
-                return true;
-            }
-        }
+        string s = countDigits(n);
+        for (int p = 1; p <= 1e9; p <<= 1)
+            if (s == countDigits(p)) return true;
         return false;
     }
 
 private:
-    string f(int x) {
-        char cnt[10] = {};
-        while (x > 0) {
-            cnt[x % 10]++;
+    string countDigits(int x) {
+        string res(10, 0);
+        while (x) {
+            res[x % 10]++;
             x /= 10;
         }
-        return string(cnt, cnt + 10);
+        return res;
     }
 };
