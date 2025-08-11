db44a4887f32fd0a11af1ec3edb43f59b0647eca
shsaish006 <shsaish006@gmail.com>
2025-08-11 10:27:29 +0530

    Time: 60 ms (37.77%) | Memory: 147.9 MB (45.21%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
-        vector<int> powers;
-        while (n) {
-            int x = n & -n;
-            powers.push_back(x);
-            n -= x;
-        }
-        vector<int> ans;
         const int mod = 1e9 + 7;
-        for (const auto& q : queries) {
-            int l = q[0], r = q[1];
-            long long x = 1;
-            for (int j = l; j <= r; ++j) {
-                x = x * powers[j] % mod;
-            }
-            ans.push_back(x);
+        vector<int> p;
+        while (n) {
+            p.push_back(n & -n);
+            n -= n & -n;
         }
-        return ans;
+        int m = p.size();
+        vector<long long> pre(m + 1, 1);
+        for (int i = 0; i < m; ++i) pre[i + 1] = pre[i] * p[i] % mod;
+        vector<int> res;
+        for (auto &q : queries)
+            res.push_back(pre[q[1] + 1] * powmod(pre[q[0]], mod - 2, mod) % mod);
+        return res;
+    }
+private:
+    long long powmod(long long a, long long b, int mod) {
+        long long r = 1;
+        while (b) {
+            if (b & 1) r = r * a % mod;
+            a = a * a % mod;
+            b >>= 1;
+        }
+        return r;
     }
 };
