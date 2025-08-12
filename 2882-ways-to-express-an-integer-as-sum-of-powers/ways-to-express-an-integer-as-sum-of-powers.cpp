2fa22f1728d02994cc0052c22dcf63dcb5a9cbef
shsaish006 <shsaish006@gmail.com>
2025-08-12 11:13:41 +0530

    Time: 92 ms (71.02%) | Memory: 9.7 MB (95.21%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int numberOfWays(int n, int x) {
-        const int mod = 1e9 + 7;
-        int f[n + 1][n + 1];
-        memset(f, 0, sizeof(f));
-        f[0][0] = 1;
-        for (int i = 1; i <= n; ++i) {
-            long long k = (long long) pow(i, x);
-            for (int j = 0; j <= n; ++j) {
-                f[i][j] = f[i - 1][j];
+    int numberOfWays(int a, int b) {
+        const int c = 1e9 + 7;
+        int d[a + 1][a + 1];
+        memset(d, 0, sizeof(d));
+        d[0][0] = 1;
+        for (int i = 1; i <= a; ++i) {
+            long long k = (long long)pow(i, b);
+            for (int j = 0; j <= a; ++j) {
+                d[i][j] = d[i - 1][j];
                 if (k <= j) {
-                    f[i][j] = (f[i][j] + f[i - 1][j - k]) % mod;
+                    d[i][j] = (d[i][j] + d[i - 1][j - k]) % c;
                 }
             }
         }
-        return f[n][n];
+        return d[a][a];
     }
 
     
