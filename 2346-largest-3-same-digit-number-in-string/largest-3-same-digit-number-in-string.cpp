ba41faf51356bd59131bba37581d8daf78569194
shsaish006 <shsaish006@gmail.com>
2025-08-14 11:40:37 +0530

    Time: 2 ms (16.00%) | Memory: 9.1 MB (52.45%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        for (char i = '9'; i >= '0'; --i) {
-            string s(3, i);
-            if (num.find(s) != string::npos) {
-                return s;
+        string res;
+        for (int i = 0; i + 2 < num.size(); i++) {
+            if (num[i] == num[i+1] && num[i] == num[i+2]) {
+                res = max(res, num.substr(i, 3));
             }
         }
-        return "";
+        return res;
     }
 };
