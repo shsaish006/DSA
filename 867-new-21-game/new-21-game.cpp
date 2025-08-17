59a3789aa20643e1d50eabbdfb7722948e069772
shsaish006 <shsaish006@gmail.com>
2025-08-17 12:07:57 +0530

    Time: 39 ms (6.59%) | Memory: 14.4 MB (6.35%) - LeetSync

--- 867-new-21-game/new-21-game.cpp
+++ 867-new-21-game/new-21-game.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    double new21Game(int n, int k, int maxPts) {
-        vector<double> f(k);
-        function<double(int)> dfs = [&](int i) -> double {
-            if (i >= k) {
-                return i <= n ? 1 : 0;
-            }
-            if (i == k - 1) {
-                return min(n - k + 1, maxPts) * 1.0 / maxPts;
-            }
-            if (f[i]) {
-                return f[i];
-            }
-            return f[i] = dfs(i + 1) + (dfs(i + 1) - dfs(i + maxPts + 1)) / maxPts;
+    double new21Game(int a, int b, int c) {
+        vector<double> d(b);
+        function<double(int)> e = [&](int f) -> double {
+            if (f >= b) return f <= a ? 1 : 0;
+            if (f == b - 1) return min(a - b + 1, c) * 1.0 / c;
+            if (d[f]) return d[f];
+            return d[f] = e(f + 1) + (e(f + 1) - e(f + c + 1)) / c;
         };
-        return dfs(0);
+        
+        return e(0);
     }
 };
