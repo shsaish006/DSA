6e3a3e216833de1511cfaf6ba388f7b05405e85e
shsaish006 <shsaish006@gmail.com>
2025-08-20 12:32:04 +0530

    Time: 8 ms (44.13%) | Memory: 30.2 MB (70.36%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -6,14 +6,11 @@ public:
         vector<vector<int>> f(m, vector<int>(n));
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (matrix[i][j] == 0) {
-                    continue;
-                }
-                if (i == 0 || j == 0) {
+                if (matrix[i][j] == 0) continue;
+                if (i == 0 || j == 0)
                     f[i][j] = 1;
-                } else {
-                    f[i][j] = min(f[i - 1][j - 1], min(f[i - 1][j], f[i][j - 1])) + 1;
-                }
+                else
+                    f[i][j] = min({f[i-1][j-1], f[i-1][j], f[i][j-1]}) + 1;
                 ans += f[i][j];
             }
         }
