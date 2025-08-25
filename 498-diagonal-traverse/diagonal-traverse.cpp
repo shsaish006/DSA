50d3b70be2c2769288e54f21a0ccf5c534d57fbf
shsaish006 <shsaish006@gmail.com>
2025-08-25 13:00:15 +0530

    Time: 14 ms (44.71%) | Memory: 25.8 MB (38.83%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -1,23 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
         vector<int> ans;
-        vector<int> t;
+
         for (int k = 0; k < m + n - 1; ++k) {
+            vector<int> temp;
             int i = (k < n) ? 0 : k - n + 1;
             int j = (k < n) ? k : n - 1;
             while (i < m && j >= 0) {
-                t.push_back(mat[i][j]);
+                temp.push_back(mat[i][j]);
                 ++i;
                 --j;
             }
             if (k % 2 == 0) {
-                ranges::reverse(t);
+                reverse(temp.begin(), temp.end());
             }
-            ans.insert(ans.end(), t.begin(), t.end());
-            t.clear();
+            ans.insert(ans.end(), temp.begin(), temp.end());
         }
         return ans;
     }
