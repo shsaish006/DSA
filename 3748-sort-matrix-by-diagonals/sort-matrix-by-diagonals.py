04fe9dad47cb0317c721bbb22edf3e5c86f96937
shsaish006 <shsaish006@gmail.com>
2025-08-28 11:26:38 +0530

    Time: 7 ms (91.25%) | Memory: 17.8 MB (82.92%) - LeetSync

---
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+
+        # bottom-left diagonals
+        for k in range(n - 2, -1, -1):
+            i, j = k, 0
+            t = []
+            while i < n and j < n:
+                t.append(grid[i][j])
+                i += 1
+                j += 1
+            t.sort()
+            i, j = k, 0
+            while i < n and j < n:
+                grid[i][j] = t.pop()
+                i += 1
+                j += 1
+
+        # top-right diagonals
+        for k in range(n - 2, 0, -1):
+            i, j = k, n - 1
+            t = []
+            while i >= 0 and j >= 0:
+                t.append(grid[i][j])
+                i -= 1
+                j -= 1
+            t.sort()
+            i, j = k, n - 1
+            while i >= 0 and j >= 0:
+                grid[i][j] = t.pop()
+                i -= 1
+                j -= 1
+
+        return grid
