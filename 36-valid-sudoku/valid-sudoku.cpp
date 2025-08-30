97d6b2913b10a09c96ee3393371aba5e65061a9a
shsaish006 <shsaish006@gmail.com>
2025-08-30 12:17:52 +0530

    Time: 6 ms (39.73%) | Memory: 25.5 MB (31.63%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,21 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<vector<bool>> row(9, vector<bool>(9, false));
-        vector<vector<bool>> col(9, vector<bool>(9, false));
-        vector<vector<bool>> sub(9, vector<bool>(9, false));
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
+        vector<unordered_set<char>> rows(9), cols(9), boxes(9);
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 char c = board[i][j];
                 if (c == '.') continue;
-                int num = c - '0' - 1;
-                int k = i / 3 * 3 + j / 3;
-                if (row[i][num] || col[j][num] || sub[k][num]) {
+
+                int boxIndex = (i / 3) * 3 + (j / 3);
+
+                if (rows[i].count(c) || cols[j].count(c) || boxes[boxIndex].count(c))
                     return false;
-                }
-                row[i][num] = true;
-                col[j][num] = true;
-                sub[k][num] = true;
+
+                rows[i].insert(c);
+                cols[j].insert(c);
+                boxes[boxIndex].insert(c);
             }
         }
         return true;
