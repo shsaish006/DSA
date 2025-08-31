495365f9b15f154034847ad7e3bb2a6e3a781cfc
shsaish006 <shsaish006@gmail.com>
2025-08-31 10:31:31 +0530

    Time: 39 ms (97.61%) | Memory: 9 MB (8.17%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,41 +1,46 @@
-using pii = pair<int, int>;
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution {
 public:
+    bool row[9][9], col[9][9], block[3][3][9];
+    vector<pair<int,int>> t;
+    bool ok;
+
+    void dfs(vector<vector<char>>& board, int k) {
+        if (k == t.size()) {
+            ok = true;
+            return;
+        }
+        int i = t[k].first, j = t[k].second;
+        for (int v = 0; v < 9; v++) {
+            if (!row[i][v] && !col[j][v] && !block[i/3][j/3][v]) {
+                row[i][v] = col[j][v] = block[i/3][j/3][v] = true;
+                board[i][j] = v + '1';
+                dfs(board, k + 1);
+                if (ok) return;
+                row[i][v] = col[j][v] = block[i/3][j/3][v] = false;
+            }
+        }
+    }
+
     void solveSudoku(vector<vector<char>>& board) {
-        bool row[9][9] = {false};
-        bool col[9][9] = {false};
-        bool block[3][3][9] = {false};
-        bool ok = false;
-        vector<pii> t;
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
+        memset(row, 0, sizeof(row));
+        memset(col, 0, sizeof(col));
+        memset(block, 0, sizeof(block));
+        ok = false;
+        t.clear();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.') {
                     t.push_back({i, j});
                 } else {
                     int v = board[i][j] - '1';
-                    row[i][v] = col[j][v] = block[i / 3][j / 3][v] = true;
+                    row[i][v] = col[j][v] = block[i/3][j/3][v] = true;
                 }
             }
         }
-        function<void(int k)> dfs = [&](int k) {
-            if (k == t.size()) {
-                ok = true;
-                return;
-            }
-            int i = t[k].first, j = t[k].second;
-            for (int v = 0; v < 9; ++v) {
-                if (!row[i][v] && !col[j][v] && !block[i / 3][j / 3][v]) {
-                    row[i][v] = col[j][v] = block[i / 3][j / 3][v] = true;
-                    board[i][j] = v + '1';
-                    dfs(k + 1);
-                    row[i][v] = col[j][v] = block[i / 3][j / 3][v] = false;
-                }
-                if (ok) {
-                    return;
-                }
-            }
-        };
-        dfs(0);
+        dfs(board, 0);
     }
 };
