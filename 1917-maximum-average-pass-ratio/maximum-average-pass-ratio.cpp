7f19ebe6ee7d8ec39de82c740370bc0b95e5da1a
shsaish006 <shsaish006@gmail.com>
2025-09-01 12:46:24 +0530

    Time: 319 ms (45.03%) | Memory: 97.9 MB (66.37%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-        priority_queue<tuple<double, int, int>> pq;
+        auto gain = [](int a, int b) {
+            return (double)(a + 1) / (b + 1) - (double)a / b;
+        };
+        priority_queue<pair<double, pair<int,int>>> pq;
         for (auto& e : classes) {
-            int a = e[0], b = e[1];
-            double x = (double) (a + 1) / (b + 1) - (double) a / b;
-            pq.push({x, a, b});
+            pq.push({gain(e[0], e[1]), {e[0], e[1]}});
         }
         while (extraStudents--) {
-            auto [_, a, b] = pq.top();
-            pq.pop();
-            a++;
-            b++;
-            double x = (double) (a + 1) / (b + 1) - (double) a / b;
-            pq.push({x, a, b});
+            auto [g, p] = pq.top(); pq.pop();
+            int a = p.first + 1, b = p.second + 1;
+            pq.push({gain(a, b), {a, b}});
         }
         double ans = 0;
-        while (pq.size()) {
-            auto [_, a, b] = pq.top();
-            pq.pop();
-            ans += (double) a / b;
+        while (!pq.empty()) {
+            auto [g, p] = pq.top(); pq.pop();
+            ans += (double)p.first / p.second;
         }
         return ans / classes.size();
     }
