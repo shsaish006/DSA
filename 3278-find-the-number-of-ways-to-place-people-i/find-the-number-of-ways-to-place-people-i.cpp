ce5ee473bce95d74b89efb5970201cdb60be586d
shsaish006 <shsaish006@gmail.com>
2025-09-02 12:22:08 +0530

    Time: 7 ms (66.67%) | Memory: 32.7 MB (39.26%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -4,18 +4,17 @@ public:
         sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
             return a[0] < b[0] || (a[0] == b[0] && b[1] < a[1]);
         });
-        int n = points.size();
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            int y1 = points[i][1];
-            int maxY = INT_MIN;
-            for (int j = i + 1; j < n; ++j) {
-                int y2 = points[j][1];
-                if (maxY < y2 && y2 <= y1) {
-                    maxY = y2;
-                    ++ans;
+        int a = points.size();
+        int b = 0;
+        for (int c = 0; c < a; c++) {
+            int d = points[c][1];
+            int e = INT_MIN;
+            for (int f = c + 1; f < a; f++) {
+                int g = points[f][1];
+                if (e < g && g <= d) {
+                    e = g;
+                    b++;
                 }
             }
         }
-        return ans;
-    }};
+        return b;}};
