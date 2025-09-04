ff8226b9bfabb3bd29842a6ae87a53980f1038f6
shsaish006 <shsaish006@gmail.com>
2025-09-04 11:50:45 +0530

    Time: 1 ms (6.52%) | Memory: 8.7 MB (0.68%) - LeetSync

--- 3830-find-closest-person/find-closest-person.cpp
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -3,6 +3,8 @@ public:
     int findClosest(int x, int y, int z) {
         int a = abs(x - z);
         int b = abs(y - z);
-        return a == b ? 0 : (a < b ? 1 : 2);
+        if (a < b) return 1;
+        if (b < a) return 2;
+        return 0;
     }
 };
