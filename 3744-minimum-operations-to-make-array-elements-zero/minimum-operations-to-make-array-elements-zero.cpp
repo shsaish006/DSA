a7f748837df7cd5e81a529239407b301b81316d9
shsaish006 <shsaish006@gmail.com>
2025-09-06 12:34:02 +0530

    Time: 24 ms (60.15%) | Memory: 183.4 MB (64.49%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -1,24 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     long long minOperations(vector<vector<int>>& queries) {
-        auto f = [&](long long x) {
-            long long res = 0;
-            long long p = 1;
-            int i = 1;
+        auto calc = [&](long long x) {
+            long long res = 0, p = 1;
+            int d = 1;
             while (p <= x) {
-                long long cnt = min(p * 4 - 1, x) - p + 1;
-                res += cnt * i;
-                i++;
+                long long len = min(p * 4 - 1, x) - p + 1;
+                res += len * d;
                 p *= 4;
+                d++;
             }
             return res;
         };
-
         long long ans = 0;
-        for (auto& q : queries) {
-            int l = q[0], r = q[1];
-            long long s = f(r) - f(l - 1);
-            long long mx = f(r) - f(r - 1);
+        for (auto &q : queries) {
+            long long l = q[0], r = q[1];
+            long long s = calc(r) - calc(l - 1);
+            long long mx = calc(r) - calc(r - 1);
             ans += max((s + 1) / 2, mx);
         }
         return ans;
