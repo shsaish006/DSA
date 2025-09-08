64a5d11a49ca77d8b4f97b194e2f2b9af356983c
shsaish006 <shsaish006@gmail.com>
2025-09-08 09:59:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (85.27%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     vector<int> getNoZeroIntegers(int n) {
-        for (int a = 1;; ++a) {
-            int b = n - a;
-            if ((to_string(a) + to_string(b)).find('0') == -1) {
-                return {a, b};
+        auto noZero = [&](int x) {
+            while (x > 0) {
+                if (x % 10 == 0) return false;
+                x /= 10;
             }
+            return true;
+        };
+        
+        for (int a = 1; a < n; ++a) {
+            int b = n - a;
+            if (noZero(a) && noZero(b)) return {a, b};
         }
+        return {};
     }
 };
