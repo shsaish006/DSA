78383a16e4577078e107371fa64137ec6159a9ba
shsaish006 <shsaish006@gmail.com>
2025-09-10 10:22:01 +0530

    Time: 23 ms (87.06%) | Memory: 64.3 MB (94.12%) - LeetSync

--- 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.cpp
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
-        unordered_set<int> s;
-        auto check = [&](int u, int v) {
-            for (int x : languages[u - 1]) {
-                for (int y : languages[v - 1]) {
-                    if (x == y) {
-                        return true;
-                    }
+        unordered_set<int> needTeach;
+        for (auto& f : friendships) {
+            int u = f[0], v = f[1];
+            bool canCommunicate = false;
+            for (int langU : languages[u - 1]) {
+                if (find(languages[v - 1].begin(), languages[v - 1].end(), langU) != languages[v - 1].end()) {
+                    canCommunicate = true;
+                    break;
                 }
             }
-            return false;
-        };
-        for (auto& e : friendships) {
-            int u = e[0], v = e[1];
-            if (!check(u, v)) {
-                s.insert(u);
-                s.insert(v);
+            if (!canCommunicate) {
+                needTeach.insert(u);
+                needTeach.insert(v);
             }
         }
-        if (s.empty()) {
-            return 0;
-        }
-        vector<int> cnt(n + 1);
-        for (int u : s) {
-            for (int& l : languages[u - 1]) {
-                ++cnt[l];
+        if (needTeach.empty()) return 0;
+        vector<int> langCount(n + 1, 0);
+        for (int person : needTeach) {
+            for (int lang : languages[person - 1]) {
+                langCount[lang]++;
             }
         }
-        return s.size() - ranges::max(cnt);
+        int best = 0;
+        for (int i = 1; i <= n; i++) {
+            best = max(best, langCount[i]);
+        }
+        return (int)needTeach.size() - best;
     }
 };
