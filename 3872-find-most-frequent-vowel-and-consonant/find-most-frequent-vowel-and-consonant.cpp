8400bc5ff95250f8c5b68e9748698fa1fc0656a4
shsaish006 <shsaish006@gmail.com>
2025-09-13 11:28:13 +0530

    Time: 3 ms (37.49%) | Memory: 9.4 MB (81.58%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        int cnt[26]{};
-        for (char c : s) {
-            ++cnt[c - 'a'];
-        }
+        int cnt[26] = {};
+        for (char c : s) cnt[c - 'a']++;
         int a = 0, b = 0;
-        for (int i = 0; i < 26; ++i) {
-            char c = 'a' + i;
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                a = max(a, cnt[i]);
-            } else {
-                b = max(b, cnt[i]);
-            }
+        for (int i = 0; i < 26; i++) {
+            if (string("aeiou").find('a' + i) != string::npos) a = max(a, cnt[i]);
+            else b = max(b, cnt[i]);
         }
-        return a + b;
-    }};
+        return a + b;}};
