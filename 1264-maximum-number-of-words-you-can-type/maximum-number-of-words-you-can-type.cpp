d096d5c04da028d915e916706683c27b157149b2
shsaish006 <shsaish006@gmail.com>
2025-09-15 10:18:09 +0530

    Time: 3 ms (18.94%) | Memory: 8.9 MB (69.10%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
-        bool s[26]{};
-        for (char c : brokenLetters) {
-            s[c - 'a'] = true;
-        }
+        vector<bool> broken(26, false);
+        for (char c : brokenLetters) broken[c - 'a'] = true;
+        
         int ans = 0;
-        stringstream ss(text);
-        string w;
-        while (ss >> w) {
-            for (char c : w) {
-                if (s[c - 'a']) {
-                    --ans;
-                    break;
+        string word;
+        for (int i = 0; i <= text.size(); i++) {
+            if (i == text.size() || text[i] == ' ') {
+                bool ok = true;
+                for (char c : word) {
+                    if (broken[c - 'a']) { ok = false; break; }
                 }
+                if (ok) ans++;
+                word.clear();
+            } else {
+                word += text[i];
             }
-            ++ans;
-        }
-        return ans;
-    }};
+        }return ans; }};
