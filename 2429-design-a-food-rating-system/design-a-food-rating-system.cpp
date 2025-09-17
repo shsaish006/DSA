1d12c6e84bc2090bfb5cd6e338b3f075c6e95e91
shsaish006 <shsaish006@gmail.com>
2025-09-17 11:22:12 +0530

    Time: 128 ms (74.24%) | Memory: 162.6 MB (89.75%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -1,29 +1,24 @@
 class FoodRatings {
-public:
-    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
-        for (int i = 0; i < foods.size(); ++i) {
-            string food = foods[i], cuisine = cuisines[i];
-            int rating = ratings[i];
-            d[cuisine].insert({-rating, food});
-            g[food] = {rating, cuisine};
-        }
-    }
-
-    void changeRating(string food, int newRating) {
-        auto [oldRating, cuisine] = g[food];
-        g[food] = {newRating, cuisine};
-        d[cuisine].erase({-oldRating, food});
-        d[cuisine].insert({-newRating, food});
-    }
-
-    string highestRated(string cuisine) {
-        return d[cuisine].begin()->second;
-    }
-
-private:
     unordered_map<string, set<pair<int, string>>> d;
     unordered_map<string, pair<int, string>> g;
+public:
+    FoodRatings(vector<string>& f, vector<string>& c, vector<int>& r) {
+        for (int i = 0; i < f.size(); i++) {
+            d[c[i]].insert({-r[i], f[i]});
+            g[f[i]] = {r[i], c[i]};
+        }
+    }
+    void changeRating(string food, int nr) {
+        auto [or_, c] = g[food];
+        g[food] = {nr, c};
+        d[c].erase({-or_, food});
+        d[c].insert({-nr, food});
+    }
+    string highestRated(string c) {
+        return d[c].begin()->second;
+    }
 };
+
 
 /**
  * Your FoodRatings object will be instantiated and called as such:
