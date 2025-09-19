84f5f6e0cd8c49b930f6a5a7b86f599f96739ae4
shsaish006 <shsaish006@gmail.com>
2025-09-19 10:56:03 +0530

    Time: 81 ms (73.94%) | Memory: 124.5 MB (86.70%) - LeetSync

--- 3797-design-spreadsheet/design-spreadsheet.cpp
+++ 3797-design-spreadsheet/design-spreadsheet.cpp
@@ -1,32 +1,18 @@
 class Spreadsheet {
-private:
     unordered_map<string, int> d;
-
 public:
-    Spreadsheet(int rows) {}
-
-    void setCell(string cell, int value) {
-        d[cell] = value;
-    }
-
-    void resetCell(string cell) {
-        d.erase(cell);
-    }
-
-    int getValue(string formula) {
-        int ans = 0;
-        stringstream ss(formula.substr(1));
-        string cell;
-        while (getline(ss, cell, '+')) {
-            if (isdigit(cell[0])) {
-                ans += stoi(cell);
-            } else {
-                ans += d.count(cell) ? d[cell] : 0;
-            }
-        }
-        return ans;
+    Spreadsheet(int) {}
+    void setCell(string c, int v) { d[c] = v; }
+    void resetCell(string c) { d.erase(c); }
+    int getValue(string f) {
+        int r = 0;
+        string t;
+        stringstream ss(f.substr(1));
+        while (getline(ss, t, '+')) r += isdigit(t[0]) ? stoi(t) : d[t];
+        return r;
     }
 };
+
 
 /**
  * Your Spreadsheet object will be instantiated and called as such:
