2ceb6f7f06e0543d76390b4a07dd4c72199d4a9e
shsaish006 <shsaish006@gmail.com>
2025-09-23 10:45:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (38.53%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    int compareVersion(string version1, string version2) {
-        int m = version1.size(), n = version2.size();
-        for (int i = 0, j = 0; i < m || j < n; ++i, ++j) {
-            int a = 0, b = 0;
-            while (i < m && version1[i] != '.') {
-                a = a * 10 + (version1[i++] - '0');
-            }
-            while (j < n && version2[j] != '.') {
-                b = b * 10 + (version2[j++] - '0');
-            }
-            if (a != b) {
-                return a < b ? -1 : 1;
-            }
+    int compareVersion(string v1, string v2) {
+        vector<int> a, b;
+        split(v1, a);
+        split(v2, b);
+        int n = max(a.size(), b.size());
+        for (int i = 0; i < n; i++) {
+            int x = i < a.size() ? a[i] : 0;
+            int y = i < b.size() ? b[i] : 0;
+            if (x != y) return x < y ? -1 : 1;
         }
         return 0;
-    }};
+    }
+private:
+    void split(const string& s, vector<int>& out) {
+        int num = 0;
+        for (char c : s) {
+            if (c == '.') {
+                out.push_back(num);
+                num = 0;
+            } else {
+                num = num * 10 + (c - '0');
+            }
+        }
+        out.push_back(num);
+    }
+};
