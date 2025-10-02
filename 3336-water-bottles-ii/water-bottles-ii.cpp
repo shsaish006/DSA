cf574e4f5bb78deb2df88e452bb514a39d64aded
shsaish006 <shsaish006@gmail.com>
2025-10-02 11:07:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (44.88%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.cpp
+++ 3336-water-bottles-ii/water-bottles-ii.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int maxBottlesDrunk(int numBottles, int numExchange) {
-        int ans = numBottles;
-        while (numBottles >= numExchange) {
-            numBottles -= numExchange;
-            ++numExchange;
-            ++ans;
-            ++numBottles;
+        int res = 0, empty = 0;
+        while (numBottles > 0) {
+            res += numBottles;
+            empty += numBottles;
+            numBottles = 0;
+            if (empty >= numExchange) {
+                empty -= numExchange;
+                numBottles = 1;
+                numExchange++;
+            }
         }
-        return ans;
-    }};
+        return res;
+    }
+};
