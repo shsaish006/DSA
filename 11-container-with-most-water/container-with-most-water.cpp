e96933ab91cae6620496573fa9213f14094436fe
shsaish006 <shsaish006@gmail.com>
2025-10-04 22:35:21 +0530

    Time: 4 ms (21.88%) | Memory: 63 MB (12.56%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,20 +1,11 @@
-//SHIVAM
 class Solution {
 public:
-    int maxArea(vector<int>&H) {
-        int i=0,j=H.size()-1;
-        int ans=0;
-        while(i<j){
-            int t=min(H[i],H[j])*(j-i);
-            ans=max(ans,t);
-            if(H[i]<H[j]){
-                ++i;
-            }else{
-                --j;
-            }
+    int maxArea(vector<int>& H) {
+        int n = H.size(), ans = 0;
+        for (int l = 0, r = n - 1; l < r;) {
+            ans = max(ans, min(H[l], H[r]) * (r - l));
+            H[l] < H[r] ? ++l : --r;
         }
         return ans;
-        
-        
     }
 };
