e1ebf8a830dd8c51c0a1e94d160d2f4e421e3474
shsaish006 <shsaish006@gmail.com>
2025-10-11 10:56:36 +0530

    Time: 373 ms (42.29%) | Memory: 208.9 MB (53.38%) - LeetSync

--- 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
@@ -1,33 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
+    unordered_map<int,int> cnt;
+    vector<long long> f;
+    vector<int> p,nx;
+    int n;
+    long long dfs(int i){
+        if(i>=n)return 0;
+        if(f[i])return f[i];
+        long long a=dfs(i+cnt[p[i]]);
+        long long b=1LL*p[i]*cnt[p[i]]+dfs(nx[i]);
+        return f[i]=max(a,b);
+    }
 public:
-    long long maximumTotalDamage(vector<int>& power) {
-        sort(power.begin(), power.end());
-        this->power = power;
-        n = power.size();
-        f.resize(n);
-        nxt.resize(n);
-        for (int i = 0; i < n; ++i) {
-            cnt[power[i]]++;
-            nxt[i] = upper_bound(power.begin() + i + 1, power.end(), power[i] + 2) - power.begin();
+    long long maximumTotalDamage(vector<int>& a){
+        sort(a.begin(),a.end());
+        p=a;n=a.size();
+        f.resize(n);nx.resize(n);
+        for(int i=0;i<n;i++){
+            cnt[p[i]]++;
+            nx[i]=upper_bound(p.begin()+i+1,p.end(),p[i]+2)-p.begin();
         }
         return dfs(0);
     }
-
-private:
-    unordered_map<int, int> cnt;
-    vector<long long> f;
-    vector<int> power;
-    vector<int> nxt;
-    int n;
-
-    long long dfs(int i) {
-        if (i >= n) {
-            return 0;
-        }
-        if (f[i]) {
-            return f[i];
-        }
-        long long a = dfs(i + cnt[power[i]]);
-        long long b = 1LL * power[i] * cnt[power[i]] + dfs(nxt[i]);
-        return f[i] = max(a, b);
-    }};
+};
