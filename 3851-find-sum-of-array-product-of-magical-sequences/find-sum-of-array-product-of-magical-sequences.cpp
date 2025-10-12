8155d10b0bfe5a2c15236b14fe4ee4628f425469
shsaish006 <shsaish006@gmail.com>
2025-10-12 12:35:26 +0530

    Time: 194 ms (26.53%) | Memory: 146 MB (12.25%) - LeetSync

--- 3851-find-sum-of-array-product-of-magical-sequences/find-sum-of-array-product-of-magical-sequences.cpp
+++ 3851-find-sum-of-array-product-of-magical-sequences/find-sum-of-array-product-of-magical-sequences.cpp
@@ -1,60 +1,55 @@
-#include <bits/stdc++.h>
-using namespace std;
 using ll = long long;
-const int MOD = 1e9+7;
-ll modpow(ll a,ll e){ll r=1%MOD;while(e){if(e&1)r=r*a%MOD;a=a*a%MOD;e>>=1;}return r;}
+const int mod = 1e9+7;
+ll pwr(ll a,ll b){ll r=1%mod;while(b){if(b&1)r=r*a%mod;a=a*a%mod;b>>=1;}return r;}
 class Solution {
 public:
-    int magicalSum(int m, int k, vector<int>& nums) {
-        int n = nums.size();
-        int maxPos = n + 6;
-        vector<vector<ll>> powv(n, vector<ll>(m+1,1));
-        for(int i=0;i<n;i++) for(int t=1;t<=m;t++) powv[i][t] = powv[i][t-1] * (nums[i]%MOD) % MOD;
-        vector<ll> fact(m+1,1), invfact(m+1,1);
-        for(int i=1;i<=m;i++) fact[i]=fact[i-1]*i%MOD;
-        invfact[m]=modpow(fact[m], MOD-2);
-        for(int i=m;i>0;i--) invfact[i-1]=invfact[i]*i%MOD;
-        int U = m, C = m, O = m;
-        vector<vector<vector<ll>>> dp(U+1, vector<vector<ll>>(C+1, vector<ll>(O+1,0)));
-        dp[0][0][0]=1;
-        for(int pos=0; pos<maxPos; pos++){
-            vector<vector<vector<ll>>> ndp(U+1, vector<vector<ll>>(C+1, vector<ll>(O+1,0)));
-            for(int used=0; used<=U; used++){
-                for(int carry=0; carry<=C; carry++){
-                    for(int ones=0; ones<=O; ones++){
-                        ll cur = dp[used][carry][ones];
-                        if(!cur) continue;
-                        if(pos < n){
-                            for(int take=0; take+used<=U; take++){
-                                int s = take + carry;
-                                int bit = s&1;
-                                int nc = s>>1;
-                                int nou = used + take;
-                                int non = ones + bit;
-                                if(nc > C || non > O) continue;
-                                ll add = cur * powv[pos][take] % MOD * invfact[take] % MOD;
-                                ndp[nou][nc][non] += add;
-                                if(ndp[nou][nc][non] >= MOD) ndp[nou][nc][non]-=MOD;
+    int magicalSum(int a, int b, vector<int>& c) {
+        int d = c.size(), e = d + 6;
+        vector<vector<ll>> f(d, vector<ll>(a+1,1));
+        for(int i=0;i<d;i++) for(int j=1;j<=a;j++) f[i][j]=f[i][j-1]*(c[i]%mod)%mod;
+        vector<ll> g(a+1,1), h(a+1,1);
+        for(int i=1;i<=a;i++) g[i]=g[i-1]*i%mod;
+        h[a]=pwr(g[a],mod-2);
+        for(int i=a;i>0;i--) h[i-1]=h[i]*i%mod;
+        int idx1=a, idx2=a, cnt=a;
+        vector<vector<vector<ll>>> s(idx1+1, vector<vector<ll>>(idx2+1, vector<ll>(cnt+1,0)));
+        s[0][0][0]=1;
+        for(int i=0;i<e;i++){
+            vector<vector<vector<ll>>> o(idx1+1, vector<vector<ll>>(idx2+1, vector<ll>(cnt+1,0)));
+            for(int j=0;j<=idx1;j++){
+                for(int k=0;k<=idx2;k++){
+                    for(int l=0;l<=cnt;l++){
+                        ll val=s[j][k][l];
+                        if(!val) continue;
+                        if(i<d){
+                            for(int m=0;m+j<=idx1;m++){
+                                int n=m+k;
+                                int q=n&1;
+                                int r=n>>1;
+                                int u=j+m;
+                                int v=l+q;
+                                if(r>idx2||v>cnt) continue;
+                                ll w=val*f[i][m]%mod*h[m]%mod;
+                                o[u][r][v]+=w;
+                                if(o[u][r][v]>=mod) o[u][r][v]-=mod;
                             }
                         } else {
-                            int s = carry;
-                            int bit = s&1;
-                            int nc = s>>1;
-                            int nou = used;
-                            int non = ones + bit;
-                            if(nc > C || non > O) continue;
-                            ndp[nou][nc][non] += cur;
-                            if(ndp[nou][nc][non] >= MOD) ndp[nou][nc][non]-=MOD;
+                            int n=k;
+                            int q=n&1;
+                            int r=n>>1;
+                            int u=j;
+                            int v=l+q;
+                            if(r>idx2||v>cnt) continue;
+                            o[u][r][v]+=val;
+                            if(o[u][r][v]>=mod) o[u][r][v]-=mod;
                         }
                     }
                 }
             }
-            dp.swap(ndp);
+            s.swap(o);
         }
-        ll res = dp[m][0][k] % MOD;
-        res = res * fact[m] % MOD;
-        return (int)res;
+        ll ans=s[a][0][b]%mod;
+        ans=ans*g[a]%mod;
+        return (int)ans;
     }
-
-    
 };
