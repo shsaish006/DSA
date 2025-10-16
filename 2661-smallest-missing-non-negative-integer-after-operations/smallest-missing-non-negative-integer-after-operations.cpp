22798542a3a1b65abf96d67f401a0c62028c8809
shsaish006 <shsaish006@gmail.com>
2025-10-16 16:38:00 +0530

    Time: 8 ms (69.73%) | Memory: 120.5 MB (89.16%) - LeetSync

--- 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.cpp
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int value) {
-        int cnt[value];
-        memset(cnt, 0, sizeof(cnt));
-        for (int x : nums) {
-            ++cnt[(x % value + value) % value];
-        }
-        for (int i = 0;; ++i) {
-            if (cnt[i % value]-- == 0) {
-                return i;
-            }
-        }
+        vector<int> cnt(value);
+        for (int x : nums) cnt[(x % value + value) % value]++;
+        for (int i = 0;; i++) if (cnt[i % value]-- == 0) return i;
     }
 };
