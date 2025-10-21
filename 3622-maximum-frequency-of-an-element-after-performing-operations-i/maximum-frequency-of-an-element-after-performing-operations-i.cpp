0d7a2e0fc17565142d809bfe38c662dc0e6923c1
shsaish006 <shsaish006@gmail.com>
2025-10-21 19:23:59 +0530

    Time: 787 ms (16.27%) | Memory: 203.1 MB (23.06%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+    int maxFrequency(vector<int>& a, int k, int o) {
         unordered_map<int, int> cnt;
         map<int, int> d;
-
-        for (int x : nums) {
-            cnt[x]++;
-            d[x];
-            d[x - k]++;
-            d[x + k + 1]--;
+        for (int val : a) {
+            cnt[val]++;
+            d[val];
+            d[val - k]++;
+            d[val + k + 1]--;
         }
-
         int ans = 0, s = 0;
-        for (const auto& [x, t] : d) {
-            s += t;
-            ans = max(ans, min(s, cnt[x] + numOperations));
+        for (auto &p : d) {
+            s += p.second;
+            ans = max(ans, min(s, cnt[p.first] + o));
         }
-        
-return ans; }};
+        return ans;
+    }};
