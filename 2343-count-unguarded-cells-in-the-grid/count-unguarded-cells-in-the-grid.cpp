67c8e8b833fc18aed960f5c05dd2ad4cb9419dc7
shsaish006 <shsaish006@gmail.com>
2025-11-02 11:17:01 +0530

    Time: 51 ms (84.40%) | Memory: 173.3 MB (48.17%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        int g[m][n];
-        memset(g, 0, sizeof(g));
-        for (auto& e : guards) {
-            g[e[0]][e[1]] = 2;
-        }
-        for (auto& e : walls) {
-            g[e[0]][e[1]] = 2;
-        }
-        int dirs[5] = {-1, 0, 1, 0, -1};
-        for (auto& e : guards) {
-            for (int k = 0; k < 4; ++k) {
-                int x = e[0], y = e[1];
-                int a = dirs[k], b = dirs[k + 1];
-                while (x + a >= 0 && x + a < m && y + b >= 0 && y + b < n && g[x + a][y + b] < 2) {
-                    x += a;
-                    y += b;
-                    g[x][y] = 1;
+        vector<vector<int>> g(m, vector<int>(n, 0));
+        for (auto &e : guards) g[e[0]][e[1]] = 2;
+        for (auto &e : walls) g[e[0]][e[1]] = 3;
+
+        vector<pair<int,int>> dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        for (auto &e : guards) {
+            for (auto [dx, dy] : dirs) {
+                int x = e[0] + dx, y = e[1] + dy;
+                while (x >= 0 && x < m && y >= 0 && y < n && g[x][y] != 2 && g[x][y] != 3) {
+                    if (g[x][y] == 0) g[x][y] = 1;
+                    x += dx;
+                    y += dy;
                 }
             }
         }
+
         int ans = 0;
-        for (auto& row : g) {
-            ans += count(row, row + n, 0);
-        }
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (g[i][j] == 0)
+                    ++ans;
         return ans;
     }
 };
+
