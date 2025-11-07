3e615fa258b081a754ae01be221a7209f7f337d8
shsaish006 <shsaish006@gmail.com>
2025-11-07 22:22:08 +0530

    Time: 107 ms (55.20%) | Memory: 284.4 MB (37.85%) - LeetSync

--- 2618-maximize-the-minimum-powered-city/maximize-the-minimum-powered-city.cpp
+++ 2618-maximize-the-minimum-powered-city/maximize-the-minimum-powered-city.cpp
@@ -1,48 +1,35 @@
 class Solution {
 public:
-    long long maxPower(vector<int>& stations, int r, int k) {
-        int n = stations.size();
-        long long d[n + 1];
-        memset(d, 0, sizeof d);
-        for (int i = 0; i < n; ++i) {
-            int left = max(0, i - r), right = min(i + r, n - 1);
-            d[left] += stations[i];
-            d[right + 1] -= stations[i];
+    long long maxPower(vector<int>& a, int r, int k) {
+        int n = a.size();
+        vector<long long> d(n + 2), s(n + 2);
+        for (int i = 0; i < n; i++) {
+            int l = max(0, i - r), rr = min(n, i + r + 1);
+            d[l] += a[i];
+            d[rr] -= a[i];
         }
-        long long s[n + 1];
-        s[0] = d[0];
-        for (int i = 1; i < n + 1; ++i) {
-            s[i] = s[i - 1] + d[i];
-        }
-        auto check = [&](long long x, int k) {
-            memset(d, 0, sizeof d);
-            long long t = 0;
-            for (int i = 0; i < n; ++i) {
-                t += d[i];
-                long long dist = x - (s[i] + t);
-                if (dist > 0) {
-                    if (k < dist) {
-                        return false;
-                    }
-                    k -= dist;
-                    int j = min(i + r, n - 1);
-                    int left = max(0, j - r), right = min(j + r, n - 1);
-                    d[left] += dist;
-                    d[right + 1] -= dist;
-                    t += dist;
+        for (int i = 0; i < n; i++) s[i + 1] = s[i] + d[i];
+        auto ok = [&](long long x) {
+            vector<long long> diff(n + 2);
+            long long t = 0, kk = k;
+            for (int i = 0; i < n; i++) {
+                t += diff[i];
+                long long need = x - (s[i + 1] + t);
+                if (need > 0) {
+                    if (kk < need) return false;
+                    kk -= need;
+                    t += need;
+                    int rr = min(n, i + 2 * r + 1);
+                    diff[rr] -= need;
                 }
             }
             return true;
         };
-        long long left = 0, right = 1e12;
-        while (left < right) {
-            long long mid = (left + right + 1) >> 1;
-            if (check(mid, k)) {
-                left = mid;
-            } else {
-                right = mid - 1;
-            }
+        long long l = 0, rr = 1e12;
+        while (l < rr) {
+            long long m = (l + rr + 1) / 2;
+            if (ok(m)) l = m; else rr = m - 1;
         }
-        return left;
+        return l;
     }
 };
