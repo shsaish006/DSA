ad58b6c1e46341d2f34e891c2086b4adf890214e
shsaish006 <shsaish006@gmail.com>
2025-11-08 17:42:32 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (80.63%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int minimumOneBitOperations(int n) {
-        int ans = 0;
-        for (; n > 0; n >>= 1) {
-            ans ^= n;
-        }
-        return ans;
+        if (n == 0) return 0;
+        int b = 0;
+        while ((1 << (b + 1)) <= n) b++;
+        return (1 << (b + 1)) - 1 - minimumOneBitOperations(n ^ (1 << b));
     }
 };
