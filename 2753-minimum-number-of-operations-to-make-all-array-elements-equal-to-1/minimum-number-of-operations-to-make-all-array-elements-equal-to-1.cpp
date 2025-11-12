deb7fdbb7925a84621f61276e27b978515d45d17
shsaish006 <shsaish006@gmail.com>
2025-11-12 23:18:24 +0530

    Time: 0 ms (100.00%) | Memory: 31.8 MB (34.03%) - LeetSync

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums) {
-        int n = nums.size();
-        int cnt = 0;
-        for (int x : nums) {
-            if (x == 1) {
-                ++cnt;
-            }
-        }
-        if (cnt) {
-            return n - cnt;
-        }
+    int minOperations(vector<int>& a) {
+        int n = a.size(), cnt = count(a.begin(), a.end(), 1);
+        if (cnt) return n - cnt;
         int mi = n + 1;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             int g = 0;
-            for (int j = i; j < n; ++j) {
-                g = gcd(g, nums[j]);
+            for (int j = i; j < n; j++) {
+                g = gcd(g, a[j]);
                 if (g == 1) {
                     mi = min(mi, j - i + 1);
+                    break;
                 }
             }
         }
-        return mi > n ? -1 : n - 1 + mi - 1;
+        return mi > n ? -1 : (n + mi - 2);
     }
+
 };
+
+
+
+
+
+
