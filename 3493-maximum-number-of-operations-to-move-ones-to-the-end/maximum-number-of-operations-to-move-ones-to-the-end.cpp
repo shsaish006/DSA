21c887eaf1d968c731b21d89f5644e5bca357414
shsaish006 <shsaish006@gmail.com>
2025-11-13 10:41:21 +0530

    Time: 4 ms (71.38%) | Memory: 15.8 MB (75.00%) - LeetSync

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int ans = 0, cnt = 0;
-        int n = s.size();
-        for (int i = 0; i < n; ++i) {
-            if (s[i] == '1') {
-                ++cnt;
-            } else if (i && s[i - 1] == '1') {
-                ans += cnt;
-            }
+        int n=s.size(), cnt=0, ans=0;
+        for(int i=1;i<n;i++){
+            if (s[i-1]=='1') cnt++;
+            if (s[i-1]== '1' && s[i]== '0') ans+= cnt;
         }
         return ans;
+        
     }
 };
