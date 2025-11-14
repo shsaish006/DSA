c851826cb7462aadc4c854b2e33993ceceb14476
shsaish006 <shsaish006@gmail.com>
2025-11-14 22:24:46 +0530

    Time: 23 ms (73.67%) | Memory: 86.4 MB (75.24%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> mat(n, vector<int>(n));
-        for (auto& q : queries) {
-            int x1 = q[0], y1 = q[1], x2 = q[2], y2 = q[3];
-            mat[x1][y1]++;
-            if (x2 + 1 < n) {
-                mat[x2 + 1][y1]--;
-            }
-            if (y2 + 1 < n) {
-                mat[x1][y2 + 1]--;
-            }
-            if (x2 + 1 < n && y2 + 1 < n) {
-                mat[x2 + 1][y2 + 1]++;
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& q) {
+        vector<vector<int>> a(n, vector<int>(n, 0));
+
+        for (auto &v : q) {
+            int x1 = v[0], y1 = v[1], x2 = v[2], y2 = v[3];
+            a[x1][y1]++;
+
+            if (x2 + 1 < n) a[x2 + 1][y1]--;
+            if (y2 + 1 < n) a[x1][y2 + 1]--;
+            if (x2 + 1 < n && y2 + 1 < n) a[x2 + 1][y2 + 1]++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int u = (i ? a[i-1][j] : 0);
+                int l = (j ? a[i][j-1] : 0);
+                int d = (i && j ? a[i-1][j-1] : 0);
+                a[i][j] = a[i][j] + u + l - d;
             }
         }
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i > 0) {
-                    mat[i][j] += mat[i - 1][j];
-                }
-                if (j > 0) {
-                    mat[i][j] += mat[i][j - 1];
-                }
-                if (i > 0 && j > 0) {
-                    mat[i][j] -= mat[i - 1][j - 1];
-                }
-            }
-        }
-        return mat;
+
+        return a;
     }
 };
