960840b08d6f1b574a59211d91295990530347e1
shsaish006 <shsaish006@gmail.com>
2025-11-20 11:25:22 +0530

    Time: 5 ms (56.79%) | Memory: 21.7 MB (76.13%) - LeetSync

--- 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.cpp
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int intersectionSizeTwo(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [&](vector<int>& a, vector<int>& b) {
-            return a[1] == b[1] ? a[0] > b[0] : a[1] < b[1];
+    int intersectionSizeTwo(vector<vector<int>>& a) {
+        sort(a.begin(), a.end(), [&](vector<int>& s, vector<int>& o) {
+            if (s[1] == o[1]) return s[0] > o[0];
+            return s[1] < o[1];
         });
-        int ans = 0;
-        int s = -1, e = -1;
-        for (auto& v : intervals) {
-            int a = v[0], b = v[1];
-            if (a <= s) continue;
-            if (a > e) {
+
+
+
+
+        int s = -1, e = -1, ans = 0;
+        for (auto &v : a) {
+            int x = v[0], y = v[1];
+            if (x <= s) continue;
+            if (x > e) {
                 ans += 2;
-                s = b - 1;
-                e = b;
+                s = y - 1;
+                e = y;
             } else {
+
+                
                 ans += 1;
                 s = e;
-                e = b;
+                e = y;
             }
-
-            
         }
         return ans;
     }
