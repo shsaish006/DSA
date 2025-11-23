100fe470bed45a2084ad8a9ee1caf120757b219d
shsaish006 <shsaish006@gmail.com>
2025-11-23 12:25:27 +0530

    Time: 36 ms (44.83%) | Memory: 37.1 MB (43.08%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int maxSumDivThree(vector<int>& a) {
-        vector<int> b(3,-1e9);
-        b[0]=0;
-        for (int c :a) {
-            vector<int> d(b);
-            for (int e = 0; e < 3; e++) {
-                d[(e+c%3) % 3] =max(d[(e+c%3)%3],b[e] + c);
+    int maxSumDivThree(vector<int>& nums) {
+        const int inf = -1e9;
+        vector<int> dp(3, inf);
+        dp[0] = 0;
+
+        for(int x : nums) {
+            vector<int> temp = dp;
+            for(int j = 0; j < 3; j++) {
+                int nxt = (j + x) % 3;
+                temp[nxt] = max(temp[nxt], dp[j] + x);
             }
-            b=d;
+            dp = temp;
         }
-        return b[0];
+        return dp[0];
     }
 };
-                           
