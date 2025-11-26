2192f7295762e1768a63b9aaa3971909fa4aaa67
shsaish006 <shsaish006@gmail.com>
2025-11-26 17:48:02 +0530

    Time: 162 ms (91.23%) | Memory: 85.4 MB (91.94%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     int numberOfPaths(vector<vector<int>>& grid, int K) {
-        const int mod = 1e9 + 7;
+        int mod = 1000000007;
         int m = grid.size(), n = grid[0].size();
-        int f[m][n][K];
-        memset(f, 0, sizeof(f));
-        f[0][0][grid[0][0] % K] = 1;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                for (int k = 0; k < K; ++k) {
-                    int k0 = ((k - grid[i][j] % K) + K) % K;
-                    if (i > 0) {
-                        f[i][j][k] = (f[i][j][k] + f[i - 1][j][k0]) % mod;
-                    }
-                    if (j > 0) {
-                        f[i][j][k] = (f[i][j][k] + f[i][j - 1][k0]) % mod;
-                    }
+        
+        vector<vector<vector<int>>> dp(2, vector<vector<int>>(n, vector<int>(K, 0)));
+        
+        dp[0][0][grid[0][0] % K] = 1;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) continue;
+                for(int k = 0; k < K; k++) dp[i % 2][j][k] = 0;
+                
+                for(int k = 0; k < K; k++) {
+                    int curr = (k - grid[i][j] % K + K) % K;
+                    
+                    if(i > 0)
+                        dp[i % 2][j][k] = (dp[i % 2][j][k] + dp[(i - 1) % 2][j][curr]) % mod;
+                    
+                    if(j > 0)
+                        dp[i % 2][j][k] = (dp[i % 2][j][k] + dp[i % 2][j - 1][curr]) % mod;
                 }
             }
         }
-        return f[m - 1][n - 1][0];
+        
+        return dp[(m - 1) % 2][n - 1][0];
     }
 };
-
