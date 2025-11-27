b42984387e5302f51084b0454dc25356f1f09ddf
shsaish006 <shsaish006@gmail.com>
2025-11-27 20:10:30 +0530

    Time: 0 ms (100.00%) | Memory: 162.2 MB (90.10%) - LeetSync

--- 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    long long maxSubarraySum(vector<int>& nums, int k) {
-        using ll = long long;
-        ll inf = 1e18;
-        vector<ll> f(k, inf);
-        ll ans = -inf;
-        ll s = 0;
-        f[k - 1] = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            s += nums[i];
-            ans = max(ans, s - f[i % k]);
-            f[i % k] = min(f[i % k], s);
+    long long maxSubarraySum(vector<int>& vals, int k) {
+        long long inf = 4e18;
+        vector<long long> mp(k, inf);
+        
+        long long s = 0, ans = -inf;
+        mp[0] = 0;
+        
+        for(int idx = 0; idx < vals.size(); idx++) {
+            s += vals[idx];
+            int curr = (idx + 1) % k;
+            ans = max(ans, s - mp[curr]);
+            mp[curr] = min(mp[curr], s);
         }
         return ans;
     }
