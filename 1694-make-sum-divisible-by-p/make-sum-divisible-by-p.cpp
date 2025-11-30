80c8702caa0229fa719431ac5b8ed6bb661caa75
shsaish006 <shsaish006@gmail.com>
2025-11-30 21:39:39 +0530

    Time: 79 ms (63.11%) | Memory: 87.7 MB (92.18%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-        int k = 0;
-        for (int& x : nums) {
-            k = (k + x) % p;
-        }
-        if (k == 0) {
-            return 0;
-        }
-        unordered_map<int, int> last;
-        last[0] = -1;
+        long total = 0;
+        for (int x : nums) total = (total + x) % p;
+
+        if (total == 0) return 0;   // already divisible
+
+        unordered_map<int,int> pos;
+        pos[0] = -1;
+
+        long pref = 0;
         int n = nums.size();
-        int ans = n;
-        int cur = 0;
-        for (int i = 0; i < n; ++i) {
-            cur = (cur + nums[i]) % p;
-            int target = (cur - k + p) % p;
-            if (last.count(target)) {
-                ans = min(ans, i - last[target]);
+        int best = n;
+
+        for (int i = 0; i < n; i++) {
+            pref = (pref + nums[i]) % p;
+
+            long need = (pref - total + p) % p;
+
+            auto it = pos.find(need);
+            if (it != pos.end()) {
+                best = min(best, i - it->second);
             }
-            last[cur] = i;
+
+            pos[pref] = i;
         }
-        return ans == n ? -1 : ans;
+
+        return best == n ? -1 : best;
     }
 };
