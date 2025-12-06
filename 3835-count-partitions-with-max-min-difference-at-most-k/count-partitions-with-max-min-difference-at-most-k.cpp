bc406018e744c40a9a37c0de1293569bf6779522
shsaish006 <shsaish006@gmail.com>
2025-12-06 22:03:19 +0530

    Time: 20 ms (99.53%) | Memory: 151.2 MB (62.15%) - LeetSync

--- 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.cpp
+++ 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     int countPartitions(vector<int>& nums, int k) {
-        const int mod = 1e9 + 7;
-        multiset<int> sl;
         int n = nums.size();
-        vector<int> f(n + 1, 0), g(n + 1, 0);
+        int mod = 1000000007;
+        
+        vector<int> f(n + 1), g(n + 1);
         f[0] = 1;
         g[0] = 1;
+
+        deque<int> mn, mx;
         int l = 1;
-        for (int r = 1; r <= n; ++r) {
+
+        for(int r = 1; r <= n; r++){
             int x = nums[r - 1];
-            sl.insert(x);
-            while (*sl.rbegin() - *sl.begin() > k) {
-                sl.erase(sl.find(nums[l - 1]));
-                ++l;
+
+            while(!mn.empty() && mn.back() > x) mn.pop_back();
+            mn.push_back(x);
+
+            while(!mx.empty() && mx.back() < x) mx.pop_back();
+            mx.push_back(x);
+
+            while(!mn.empty() && !mx.empty() && mx.front() - mn.front() > k){
+                int y = nums[l - 1];
+                if(mn.front() == y) mn.pop_front();
+                if(mx.front() == y) mx.pop_front();
+                l++;
             }
-            f[r] = (g[r - 1] - (l >= 2 ? g[l - 2] : 0) + mod) % mod;
+        int sub = (l >= 2 ? g[l - 2] : 0);
+            f[r] = (g[r - 1] - sub + mod) % mod;
             g[r] = (g[r - 1] + f[r]) % mod;
         }
-        return f[n]; }};
+        return f[n];
+    }
+};
