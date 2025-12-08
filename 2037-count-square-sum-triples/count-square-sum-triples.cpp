f7aa94830532442ffd303cb1a5ef608b093456d6
shsaish006 <shsaish006@gmail.com>
2025-12-08 19:47:01 +0530

    Time: 8 ms (50.66%) | Memory: 9 MB (8.85%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int countTriples(int n) {
+        vector<int> sq(n + 1);
+        for (int i = 1; i <= n; i++) sq[i] = i * i;
         int ans = 0;
-        for (int a = 1; a < n; ++a) {
-            for (int b = 1; b < n; ++b) {
-                int x = a * a + b * b;
-                int c = static_cast<int>(sqrt(x));
-                if (c <= n && c * c == x) {
-                    ++ans;
-                }
+        for (int a = 1; a < n; a++) {
+            for (int b = 1; b < n; b++) {
+                int s = sq[a] + sq[b];
+                int c = sqrt(s);
+                if (c <= n && c * c == s) ans++;
             }
         }
+
         return ans;
+    }
 
-
-        
-    }};
+};
