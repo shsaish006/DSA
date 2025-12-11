2d6eeaf783e8e765508dc62034cf92f69ce7ac42
shsaish006 <shsaish006@gmail.com>
2025-12-11 22:35:36 +0530

    Time: 1098 ms (16.57%) | Memory: 362 MB (58.01%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.cpp
+++ 3819-count-covered-buildings/count-covered-buildings.cpp
@@ -1,19 +1,43 @@
+// class Solution {
+// public:
+//     int countCoveredBuildings(int n, vector<vector<int>>& vals) {
+//         map<int, vector<int>> a, b;
+//         for (auto &curr : vals) {
+//             a[curr[0]].push_back(curr[1]);
+//             b[curr[1]].push_back(curr[0]);
+//         }
+//         for (auto &curr : a) sort(curr.second.begin(), curr.second.end());
+//         for (auto &curr : b) sort(curr.second.begin(), curr.second.end());
+//         int cnt = 0;
+//         for (auto &curr : vals) {
+//             int x = curr[0], y = curr[1];
+//             auto &l1 = a[x];
+//             auto &l2 = b[y];
+//             if (l2.front() < x && x < l2.back() && l1.front() < y && y < l1.back()) cnt++;
+//         }
+//         return cnt;
+//     }
+// };
 class Solution {
 public:
     int countCoveredBuildings(int n, vector<vector<int>>& vals) {
-        map<int, vector<int>> a, b;
+        map<int, int> xLeft, xRight, yLeft, yRight;
         for (auto &curr : vals) {
-            a[curr[0]].push_back(curr[1]);
-            b[curr[1]].push_back(curr[0]);
+            int x = curr[0], y = curr[1];
+            if (!xLeft.count(y)) xLeft[y] = x; else xLeft[y] = min(xLeft[y], x);
+            if (!xRight.count(y)) xRight[y] = x; else xRight[y] = max(xRight[y], x);
+            if (!yLeft.count(x)) yLeft[x] = y; else yLeft[x] = min(yLeft[x], y);
+            if (!yRight.count(x)) yRight[x] = y; else yRight[x] = max(yRight[x], y);
         }
-        for (auto &curr : a) sort(curr.second.begin(), curr.second.end());
-        for (auto &curr : b) sort(curr.second.begin(), curr.second.end());
+
         int cnt = 0;
         for (auto &curr : vals) {
             int x = curr[0], y = curr[1];
-            auto &l1 = a[x];
-            auto &l2 = b[y];
-            if (l2.front() < x && x < l2.back() && l1.front() < y && y < l1.back()) cnt++;
+            bool hasLeft  = x > xLeft[y];
+            bool hasRight = x < xRight[y];
+            bool hasDown  = y > yLeft[x];
+            bool hasUp    = y < yRight[x];
+            if (hasLeft && hasRight && hasUp && hasDown) cnt++;
         }
         return cnt;
     }
