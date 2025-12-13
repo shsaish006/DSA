0d15e11af5e4b21b0410a86856e6257ab5aead46
shsaish006 <shsaish006@gmail.com>
2025-12-13 18:51:30 +0530

    Time: 10 ms (33.48%) | Memory: 68.1 MB (85.84%) - LeetSync

--- 3934-coupon-code-validator/coupon-code-validator.cpp
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
-        vector<int> idx;
-        unordered_set<string> bs = {"electronics", "grocery", "pharmacy", "restaurant"};
+    vector<string> validateCoupons(vector<string>& a,
+                                   vector<string>& b,
+                                   vector<bool>& c) {
+        unordered_set<string> d = {
+            "electronics",
+            "grocery",
+            "pharmacy",
+            "restaurant"
+        };
 
-        for (int i = 0; i < code.size(); ++i) {
-            const string& c = code[i];
-            const string& b = businessLine[i];
-            bool a = isActive[i];
-            if (a && bs.count(b) && check(c)) {
+        vector<int> idx;
+        int n = a.size();
+
+        for (int i = 0; i < n; i++) {
+            if (c[i] && d.count(b[i]) && ok(a[i])) {
                 idx.push_back(i);
             }
         }
 
         sort(idx.begin(), idx.end(), [&](int i, int j) {
-            if (businessLine[i] != businessLine[j]) return businessLine[i] < businessLine[j];
-            return code[i] < code[j];
+            if (b[i] != b[j]) return b[i] < b[j];
+            return a[i] < a[j];
         });
 
-        vector<string> ans;
+        vector<string> vals;
         for (int i : idx) {
-            ans.push_back(code[i]);
+            vals.push_back(a[i]);
         }
-        return ans;
+        return vals;
     }
 
 private:
-    bool check(const string& s) {
-        if (s.empty()) return false;
+    bool ok(string s) {
+        if (s.size() == 0) return false;
         for (char c : s) {
-            if (!isalnum(c) && c != '_') {
-                return false;
-            }
+            if (!isalnum(c) && c != '_') return false;
         }
         return true;
     }
