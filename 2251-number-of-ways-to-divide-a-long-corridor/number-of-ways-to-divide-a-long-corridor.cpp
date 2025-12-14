d19cea05b987b07273f8f76465ac5dc0c284a03b
shsaish006 <shsaish006@gmail.com>
2025-12-14 19:51:12 +0530

    Time: 4 ms (99.00%) | Memory: 33.4 MB (98.00%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int numberOfWays(string corridor) {
-        int n = corridor.size();
-        int f[n][3];
-        memset(f, -1, sizeof(f));
-        const int mod = 1e9 + 7;
-        auto dfs = [&](this auto&& dfs, int i, int k) -> int {
-            if (i >= n) {
-                return k == 2;
+        const int mod = 1000000007;
+        long long o = 0;
+        long long cnt = 0;
+        long long val = 1;
+        bool s = false;
+        for (char c : corridor) {
+            if (c == 'S') {
+                o++;
+                if (o == 2) {
+                    s = true;
+                } else if (o == 3) {
+                    val = (val * (cnt + 1)) % mod;
+                    cnt = 0;
+                    o = 1;
+                }
+            } else {
+                if (o == 2) cnt++;
             }
-            if (f[i][k] != -1) {
-                return f[i][k];
-            }
-            k += corridor[i] == 'S';
-            if (k > 2) {
-                return 0;
-            }
-            f[i][k] = dfs(i + 1, k);
-            if (k == 2) {
-                f[i][k] = (f[i][k] + dfs(i + 1, 0)) % mod;
-            }
-            return f[i][k];
-        };
-        return dfs(0, 0);
+        }
+        if (!s || o != 2) return 0;
+        return (int)val;
     }
 };
+
+
