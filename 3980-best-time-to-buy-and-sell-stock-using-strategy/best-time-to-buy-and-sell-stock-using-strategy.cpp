3cfef5d2d2aec2042875941bd454f6c0ed01cd46
shsaish006 <shsaish006@gmail.com>
2025-12-18 18:29:18 +0530

    Time: 12 ms (61.10%) | Memory: 328.1 MB (43.60%) - LeetSync

--- 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.cpp
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
-        int n = prices.size();
+    long long maxProfit(vector<int>& vals, vector<int>& o, int k) {
+        int n = vals.size();
         vector<long long> s(n + 1), t(n + 1);
         for (int i = 1; i <= n; i++) {
-            int a = prices[i - 1];
-            int b = strategy[i - 1];
+            long long a = vals[i - 1];
+            long long b = o[i - 1];
             s[i] = s[i - 1] + a * b;
             t[i] = t[i - 1] + a;
         }
-        long long ans = s[n];
+        long long curr = s[n];
         for (int i = k; i <= n; i++) {
-            ans = max(ans, s[n] - (s[i] - s[i - k]) + (t[i] - t[i - k / 2]));
+            curr = max(curr, s[n] - (s[i] - s[i - k]) + (t[i] - t[i - k / 2]));
         }
-        return ans;
+        return curr;
     }
 };
