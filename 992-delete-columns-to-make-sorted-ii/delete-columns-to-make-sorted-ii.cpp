2cc7078279d92fb726462a6e991eb5c96e5ed429
shsaish006 <shsaish006@gmail.com>
2025-12-21 12:09:02 +0530

    Time: 6 ms (7.87%) | Memory: 13.1 MB (19.10%) - LeetSync

--- 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.cpp
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int minDeletionSize(vector<string>& strs) {
-        int n = strs.size();
-        int m = strs[0].size();
-        vector<bool> st(n - 1, false);
-        int ans = 0;
-        for (int j = 0; j < m; ++j) {
-            bool mustDel = false;
-            for (int i = 0; i < n - 1; ++i) {
-                if (!st[i] && strs[i][j] > strs[i + 1][j]) {
-                    mustDel = true;
+    int minDeletionSize(vector<string>& s) {
+        int n = s.size();
+        int m = s[0].size();
+        vector<int> a(n - 1, 0);
+        int cnt = 0;
+        for (int j = 0; j < m; j++) {
+            int val = 0;
+            for (int i = 0; i < n - 1; i++) {
+                if (!a[i] && s[i][j] > s[i + 1][j]) {
+                    val = 1;
                     break;
                 }
             }
-            if (mustDel) {
-                ++ans;
+            if (val) {
+                cnt++;
             } else {
-                for (int i = 0; i < n - 1; ++i) {
-                    if (!st[i] && strs[i][j] < strs[i + 1][j]) {
-                        st[i] = true;
+                for (int i = 0; i < n - 1; i++) {
+                    if (!a[i] && s[i][j] < s[i + 1][j]) {
+                        a[i] = 1;
                     }
                 }
             }
         }
-        return ans;
+        return cnt;
     }
 };
