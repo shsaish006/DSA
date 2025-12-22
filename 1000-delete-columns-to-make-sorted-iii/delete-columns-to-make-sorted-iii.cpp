0901d07af67e27abab43fc96559a5bce925f7616
shsaish006 <shsaish006@gmail.com>
2025-12-22 20:09:08 +0530

    Time: 3 ms (78.46%) | Memory: 17.8 MB (5.13%) - LeetSync

--- 1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.cpp
+++ 1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
-    int minDeletionSize(vector<string>& strs) {
-        int n = strs[0].size();
-        vector<int> f(n, 1);
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (ranges::all_of(strs, [&](const string& s) { return s[j] <= s[i]; })) {
-                    f[i] = max(f[i], f[j] + 1);
+    int minDeletionSize(vector<string>& s) {
+        int n = s[0].size();
+        int cnt = s.size();
+        vector<vector<int>> b(n, vector<int>(n, 1));
+        for (int j = 0; j < n; ++j) {
+            for (int i = j + 1; i < n; ++i) {
+                for (int k = 0; k < cnt; ++k) {
+                    if (s[k][j] > s[k][i]) {
+                        b[j][i] = 0;
+                        break;
+                    }
                 }
             }
         }
-        return n - ranges::max(f);
+        vector<int> a(n, 1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (b[j][i]) {
+                    a[i] = max(a[i], a[j] + 1);
+                }
+            }
+        }
+        int val = 0;
+        for (int i = 0; i < n; ++i) val = max(val, a[i]);
+        return n - val;
     }
 };
