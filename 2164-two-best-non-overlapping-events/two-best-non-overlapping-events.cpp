c3d0e270eb1000e2bc65b08132a5d1ed64393941
shsaish006 <shsaish006@gmail.com>
2025-12-23 23:02:07 +0530

    Time: 66 ms (83.40%) | Memory: 158.7 MB (83.78%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-        ranges::sort(events);
+        sort(events.begin(), events.end(), [](auto& a, auto& b){
+            return a[1] < b[1];
+        });
         int n = events.size();
-        vector<int> f(n + 1);
-        for (int i = n - 1; ~i; --i) {
-            f[i] = max(f[i + 1], events[i][2]);
+        vector<int> vals(n);
+        for (int i = 0; i < n; i++) {
+            vals[i] = events[i][2];
+            if (i) vals[i] = max(vals[i], vals[i - 1]);
         }
-        int ans = 0;
-        for (const auto& e : events) {
-            int v = e[2];
-            int left = 0, right = n;
-            while (left < right) {
-                int mid = (left + right) >> 1;
-                if (events[mid][0] > e[1]) {
-                    right = mid;
-                } else {
-                    left = mid + 1;
-                }
-            }
-            if (left < n) {
-                v += f[left];
-            }
-            ans = max(ans, v);
+        int val = 0;
+        for (int i = 0; i < n; i++) {
+            int j = lower_bound(events.begin(), events.begin() + i, events[i][0],
+                [](const vector<int>& a, int b){
+                    return a[1] < b;
+                }) - events.begin() - 1;
+            int curr = events[i][2];
+            if (j >= 0) curr += vals[j];
+            val = max(val, curr);
         }
-        return ans;
+        return val;
     }
+
+
 };
+
