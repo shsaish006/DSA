18665ca01db26efeb43c7eaaa0b94d0825bc9c85
shsaish006 <shsaish006@gmail.com>
2025-12-30 20:08:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (59.27%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.cpp
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -4,43 +4,32 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         int ans = 0;
-        auto check = [&](int i, int j) {
-            if (i + 3 > m || j + 3 > n) {
-                return 0;
-            }
-            vector<int> cnt(16);
-            vector<int> row(3);
-            vector<int> col(3);
-            int a = 0, b = 0;
-            for (int x = i; x < i + 3; ++x) {
-                for (int y = j; y < j + 3; ++y) {
-                    int v = grid[x][y];
-                    if (v < 1 || v > 9 || ++cnt[v] > 1) {
-                        return 0;
+        for (int i = 1; i + 1 < m; i++) {
+            for (int j = 1; j + 1 < n; j++) {
+                if (grid[i][j] != 5) continue;
+                int a = grid[i-1][j-1], b = grid[i-1][j], c = grid[i-1][j+1];
+                int d = grid[i][j-1],     e = grid[i][j],     f = grid[i][j+1];
+                int g = grid[i+1][j-1], h = grid[i+1][j], k = grid[i+1][j+1];
+                vector<int> vals = {a,b,c,d,e,f,g,h,k};
+                vector<int> seen(10);
+                bool ok = true;
+                for (int v : vals) {
+                    if (v < 1 || v > 9 || seen[v]) {
+                        ok = false;
+                        break;
                     }
-                    row[x - i] += v;
-                    col[y - j] += v;
-                    if (x - i == y - j) {
-                        a += v;
-                    }
-                    if (x - i + y - j == 2) {
-                        b += v;
-                    }
+                    seen[v] = 1;
                 }
-            }
-            if (a != b) {
-                return 0;
-            }
-            for (int k = 0; k < 3; ++k) {
-                if (row[k] != a || col[k] != a) {
-                    return 0;
-                }
-            }
-            return 1;
-        };
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                ans += check(i, j);
+                if (!ok) continue;
+                if (a + b + c != 15) continue;
+                if (d + e + f != 15) continue;
+                if (g + h + k != 15) continue;
+                if (a + d + g != 15) continue;
+                if (b + e + h != 15) continue;
+                if (c + f + k != 15) continue;
+                if (a + e + k != 15) continue;
+                if (c + e + g != 15) continue;
+                ans++;
             }
         }
         return ans;
