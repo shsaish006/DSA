8134bb1eeef8dec85a2887f8e579088a5871e384
shsaish006 <shsaish006@gmail.com>
2025-12-31 13:49:22 +0530

    Time: 30 ms (92.67%) | Memory: 112.5 MB (88.74%) - LeetSync

--- 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.cpp
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.cpp
@@ -1,46 +1,42 @@
 class Solution {
 public:
     int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
-        int l = 1, r = cells.size();
-        int g[row][col];
-        int dirs[5] = {0, 1, 0, -1, 0};
-        auto check = [&](int k) -> bool {
-            memset(g, 0, sizeof(g));
-            for (int i = 0; i < k; ++i) {
-                g[cells[i][0] - 1][cells[i][1] - 1] = 1;
+        int n = row * col;
+        vector<int> p(n + 2), r(n + 2);
+        int s = n, t = n + 1;
+        for (int i = 0; i < n + 2; i++) p[i] = i;
+        auto f = [&](int x) {
+            while (p[x] != x) {
+                p[x] = p[p[x]];
+                x = p[x];
             }
-            queue<pair<int, int>> q;
-            for (int j = 0; j < col; ++j) {
-                if (g[0][j] == 0) {
-                    q.emplace(0, j);
-                    g[0][j] = 1;
-                }
-            }
-            while (!q.empty()) {
-                auto [x, y] = q.front();
-                q.pop();
-                if (x == row - 1) {
-                    return true;
-                }
-                for (int i = 0; i < 4; ++i) {
-                    int nx = x + dirs[i];
-                    int ny = y + dirs[i + 1];
-                    if (nx >= 0 && nx < row && ny >= 0 && ny < col && g[nx][ny] == 0) {
-                        q.emplace(nx, ny);
-                        g[nx][ny] = 1;
-                    }
-                }
-            }
-            return false;
+            return x;
         };
-        while (l < r) {
-            int mid = (l + r + 1) >> 1;
-            if (check(mid)) {
-                l = mid;
-            } else {
-                r = mid - 1;
+        auto u = [&](int a, int b) {
+            a = f(a);
+            b = f(b);
+            if (a == b) return;
+            if (r[a] < r[b]) swap(a, b);
+            p[b] = a;
+            if (r[a] == r[b]) r[a]++;
+        };
+        vector<vector<int>> g(row, vector<int>(col, 0));
+        int d[5] = {0, 1, 0, -1, 0};
+        for (int i = cells.size() - 1; i >= 0; i--) {
+            int x = cells[i][0] - 1;
+            int y = cells[i][1] - 1;
+            g[x][y] = 1;
+            int id = x * col + y;
+            if (x == 0) u(id, s);
+            if (x == row - 1) u(id, t);
+            for (int k = 0; k < 4; k++) {
+                int nx = x + d[k];
+                int ny = y + d[k + 1];
+                if (nx >= 0 && nx < row && ny >= 0 && ny < col && g[nx][ny]) {
+                    u(id, nx * col + ny);
+                }
             }
+            if (f(s) == f(t)) return i;
         }
-        return l;
-    }
-};
+        return 0;
+    }};
