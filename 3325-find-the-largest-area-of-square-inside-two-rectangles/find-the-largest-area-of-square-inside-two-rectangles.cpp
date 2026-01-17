436cbcd46e849505e49cc9e875f15d5a3cf838aa
shsaish006 <shsaish006@gmail.com>
2026-01-17 23:24:48 +0530

    Time: 91 ms (66.12%) | Memory: 60.6 MB (16.94%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
-    long long largestSquareArea(vector<vector<int>>& bottomLeft, vector<vector<int>>& topRight) {
+    struct R { int x1, y1, x2, y2; };
+
+    long long largestSquareArea(vector<vector<int>>& bottomLeft,
+                                vector<vector<int>>& topRight) {
+        int n = bottomLeft.size();
+        vector<R> a(n);
+        for(int i = 0; i < n; i++){
+            a[i].x1 = bottomLeft[i][0];
+            a[i].y1 = bottomLeft[i][1];
+            a[i].x2 = topRight[i][0];
+            a[i].y2 = topRight[i][1];
+        }
+
         long long ans = 0;
-        for (int i = 0; i < bottomLeft.size(); ++i) {
-            int x1 = bottomLeft[i][0], y1 = bottomLeft[i][1];
-            int x2 = topRight[i][0], y2 = topRight[i][1];
-            for (int j = i + 1; j < bottomLeft.size(); ++j) {
-                int x3 = bottomLeft[j][0], y3 = bottomLeft[j][1];
-                int x4 = topRight[j][0], y4 = topRight[j][1];
-                int w = min(x2, x4) - max(x1, x3);
-                int h = min(y2, y4) - max(y1, y3);
-                int e = min(w, h);
-                if (e > 0) {
-                    ans = max(ans, 1LL * e * e);
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                int lx = a[i].x1 > a[j].x1 ? a[i].x1 : a[j].x1;
+                int rx = a[i].x2 < a[j].x2 ? a[i].x2 : a[j].x2;
+                int ly = a[i].y1 > a[j].y1 ? a[i].y1 : a[j].y1;
+                int ry = a[i].y2 < a[j].y2 ? a[i].y2 : a[j].y2;
+                int w = rx - lx;
+                int h = ry - ly;
+                if(w > 0 && h > 0){
+                    int s = w < h ? w : h;
+                    long long v = 1LL * s * s;
+                    if(v > ans) ans = v;
                 }
             }
         }
         return ans;
-    }
-};
+    }};
+
+
+
+    
