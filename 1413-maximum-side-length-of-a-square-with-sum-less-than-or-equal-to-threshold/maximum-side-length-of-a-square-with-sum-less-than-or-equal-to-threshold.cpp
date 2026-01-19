eeb15e6289e21a8cc14420dc4ab21a56ab9cdc3f
shsaish006 <shsaish006@gmail.com>
2026-01-19 17:36:36 +0530

    Time: 15 ms (51.36%) | Memory: 32.6 MB (78.18%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    int maxSideLength(vector<vector<int>>& mat, int threshold) {
-        int m = mat.size(), n = mat[0].size();
-        int s[m + 1][n + 1];
-        memset(s, 0, sizeof(s));
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + mat[i - 1][j - 1];
+    int maxSideLength(vector<vector<int>>& a, int t) {
+        int m=a.size(),n=a[0].size();
+        vector<vector<int>> s(m+1,vector<int>(n+1));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                s[i][j]=s[i-1][j]+s[i][j-1]-s[i-1][j-1]+a[i-1][j-1];
             }
         }
-        auto check = [&](int k) {
-            for (int i = 0; i < m - k + 1; ++i) {
-                for (int j = 0; j < n - k + 1; ++j) {
-                    if (s[i + k][j + k] - s[i][j + k] - s[i + k][j] + s[i][j] <= threshold) {
-                        return true;
-                    }
+        int l=0,r=min(m,n);
+        while(l<r){
+            int mid=(l+r+1)>>1,ok=0;
+            for(int i=0;i+mid<=m && !ok;i++){
+                for(int j=0;j+mid<=n && !ok;j++){
+                    int x=i+mid,y=j+mid;
+                    int v=s[x][y]-s[i][y]-s[x][j]+s[i][j];
+                    if(v<=t) ok=1;
                 }
             }
-            return false;
-        };
-        int l = 0, r = min(m, n);
-        while (l < r) {
-            int mid = (l + r + 1) >> 1;
-            if (check(mid)) {
-                l = mid;
-            } else {
-                r = mid - 1;
-            }
+            if(ok) l=mid;
+            else r=mid-1;
         }
         return l;
     }
