4aece1966c25c393e9f22b17fc231e7958a8502a
Shivam Kumar Singh <shsaish006@gmail.com>
2026-01-23 22:46:56 +0530

    Time: 1351 ms (5.06%) | Memory: 364.8 MB (9.49%) - LeetSync

--- 3772-minimum-pair-removal-to-sort-array-ii/minimum-pair-removal-to-sort-array-ii.cpp
+++ 3772-minimum-pair-removal-to-sort-array-ii/minimum-pair-removal-to-sort-array-ii.cpp
@@ -1,70 +1,104 @@
+// class Solution {
+// public:
+//     int minimumPairRemoval(vector<int>& nums) {
+//         int s=nums.size();
+//         vector<long long> vals(nums.begin(),nums.end());
+//         vector<int> left(s),right(s);
+//         for(int a=0;a<s;a++){
+//             left [a]=a-1;
+//             right[a]=(a+1<s?a+1:-1);
+//         }
+//         multiset<pair<long long,int>>o;
+//         int cnt=0;
+//         for(int a=0;a+1<s;a++){
+//             if(vals[a]>vals[a+1]){
+//                 cnt++;
+//             o.insert({
+//                 vals[a]+vals[a+1],a
+//             });
+//             }
+//         }
+//         int upd=0;
+//         while(cnt>0){
+//             auto it=o.begin();
+//             long long val=it->first;
+//             int a=it->second;
+//             o.erase(it);
+//             int b=right[a];
+//             if(b==-1) continue; 
+//             if(vals[a]<=vals[b]) continue;
+//             upd++;
+//             if(left[a]!=-1 && vals[left[a]]>vals[a]) cnt--;
+//             if (vals[a]>vals[b]) cnt--;
+//             if(right[b]!=-1&& vals[b]>vals[right[b]]) cnt--;
+//             vals[a]=val;
+//             right[a]=right[b];
+//             if(right[b]!=-1 ) left[right[b]]=a;
+//             if(left[a]!=-1&&vals[left[a]]>vals[a]){
+//                 cnt++;
+//             o.insert({
+//                 vals[left[a]]+vals[a],left[a]
+//             });
+//             }
+//             if (right[a]!=-1 && vals[a]>vals[right[a]]){
+//                 cnt++;
+//             o.insert({
+//                 vals[a]+vals[right[a]],a
+//             });
+//             }
+//         }
+//         return upd;
+//     }// };
+
+
+//  ....
 class Solution {
 public:
     int minimumPairRemoval(vector<int>& nums) {
-        int n = nums.size();
+        int n = nums.size(), ans = 0;
+        vector<long long> a(nums.begin(), nums.end());
         int inv = 0;
+        for (int i = 0; i < n - 1; i++) if (a[i] > a[i + 1]) inv++;
 
-        set<pair<long long, int>> sl;
         set<int> idx;
-        vector<long long> arr(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++) idx.insert(i);
 
-        for (int i = 0; i < n; ++i) idx.insert(i);
+        set<pair<long long,int>> s;
+        for (int i = 0; i < n - 1; i++) s.insert({a[i]+a[i+1], i});
 
-        for (int i = 0; i < n - 1; ++i) {
-            if (nums[i] > nums[i + 1]) {
-                ++inv;
-            }
-            sl.insert({(long long) nums[i] + nums[i + 1], i});
-        }
-
-        int ans = 0;
         while (inv > 0) {
-            ++ans;
-
-            auto it = sl.begin();
-            long long s = it->first;
+            ans++;
+            auto it = s.begin();
+            long long val = it->first;
             int i = it->second;
-            sl.erase(it);
+            s.erase(it);
 
             auto j_it = idx.upper_bound(i);
             int j = *j_it;
 
-            if (arr[i] > arr[j]) {
-                --inv;
-            }
+            if (a[i] > a[j]) inv--;
 
             auto i_it = idx.find(i);
             if (i_it != idx.begin()) {
                 auto h_it = prev(i_it);
                 int h = *h_it;
 
-                if (arr[h] > arr[i]) {
-                    --inv;
-                }
-                sl.erase({arr[h] + arr[i], h});
-
-                if (arr[h] > s) {
-                    ++inv;
-                }
-                sl.insert({arr[h] + s, h});
+                if (a[h] > a[i]) inv--;
+                s.erase({a[h]+a[i], h});
+                if (a[h] > val) inv++;
+                s.insert({a[h]+val, h});
             }
 
             auto k_it = next(j_it);
             if (k_it != idx.end()) {
                 int k = *k_it;
-
-                if (arr[j] > arr[k]) {
-                    --inv;
-                }
-                sl.erase({arr[j] + arr[k], j});
-
-                if (s > arr[k]) {
-                    ++inv;
-                }
-                sl.insert({s + arr[k], i});
+                if (a[j] > a[k]) inv--;
+                s.erase({a[j]+a[k], j});
+                if (val > a[k]) inv++;
+                s.insert({val + a[k], i});
             }
 
-            arr[i] = s;
+            a[i] = val;
             idx.erase(j);
         }
 
