a868e5efd3aae350535a456d06082e93103e5f08
Shivam Kumar Singh <shsaish006@gmail.com>
2026-02-04 22:19:46 +0530

    Time: 7 ms (81.82%) | Memory: 132.7 MB (80.00%) - LeetSync

--- 3956-trionic-array-ii/trionic-array-ii.cpp
+++ 3956-trionic-array-ii/trionic-array-ii.cpp
@@ -1,3 +1,51 @@
+// class Solution {
+// public:
+//     long long maxSumTrionic(vector<int>& nums) {
+//         int n = nums.size();
+//         long long ans = LLONG_MIN;
+//         int i = 0;
+//         while(i < n) {
+//             int l = i;
+//             i++;
+//             while(i < n && nums[i-1] < nums[i]) i++;
+//             if(i == l+1) continue;
+
+//             int peak = i-1;
+//             long long s = nums[peak-1] + nums[peak];
+
+//             while(i < n && nums[i-1] > nums[i]) {
+//                 s += nums[i];
+//                 i++;
+//             }
+//             if(i == peak+1 || i == n) continue;
+
+//             int q = i-1;
+//             s += nums[i]; 
+//             i++;
+
+//             long long mx_right = 0, t = 0;
+//             while(i < n && nums[i-1] < nums[i]) {
+//                 t += nums[i];
+//                 mx_right = max(mx_right, t);
+//                 i++;
+//             }
+//             s += mx_right;
+
+//             long long mx_left = 0;
+//             t = 0;
+//             for(int j = peak-2; j >= l; j--) {
+//                 t += nums[j];
+//                 mx_left = max(mx_left, t);
+//             }
+//             s += mx_left;
+
+//             ans = max(ans, s);
+//             i = q;
+//         }
+//         return ans;
+//     }
+// };
+// TC 857/858
 class Solution {
 public:
     long long maxSumTrionic(vector<int>& nums) {
@@ -6,36 +54,32 @@ public:
         long long ans = LLONG_MIN;
         while (i < n) {
             int l = i;
-            i += 1;
-            while (i < n && nums[i - 1] < nums[i]) {
-                i += 1;
-            }
-            if (i == l + 1) {
-                continue;
-            }
+            i++;
+            while (i < n && nums[i-1] < nums[i]) i++;
+            if (i == l + 1) continue;
 
             int p = i - 1;
-            long long s = nums[p - 1] + nums[p];
-            while (i < n && nums[i - 1] > nums[i]) {
+            long long s = nums[p-1] + nums[p];
+            while (i < n && nums[i-1] > nums[i]) {
                 s += nums[i];
-                i += 1;
+                i++;
             }
-            if (i == p + 1 || i == n || nums[i - 1] == nums[i]) {
-                continue;
-            }
+            if (i == p + 1 || i == n || nums[i-1] == nums[i]) continue;
 
             int q = i - 1;
             s += nums[i];
-            i += 1;
+            i++;
+
             long long mx = 0, t = 0;
-            while (i < n && nums[i - 1] < nums[i]) {
+            while (i < n && nums[i-1] < nums[i]) {
                 t += nums[i];
-                i += 1;
                 mx = max(mx, t);
+                i++;
             }
             s += mx;
 
-            mx = 0, t = 0;
+            mx = 0;
+            t = 0;
             for (int j = p - 2; j >= l; j--) {
                 t += nums[j];
                 mx = max(mx, t);
@@ -47,4 +91,4 @@ public:
         }
         return ans;
     }
-};
+    };
