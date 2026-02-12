05e025cf7adb3aeab7ffcc03228efd9da1f9fb2c
Shivam Kumar Singh <shsaish006@gmail.com>
2026-02-12 21:18:13 +0530

    Time: 44 ms (96.34%) | Memory: 11.3 MB (97.56%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -1,48 +1,22 @@
-// class Solution {
-// public:
-//     int longestBalanced(string s) {
-//         int n=s.size();
-//         int ans=0;
-//         for(int i=0;i<n;i++){
-//             vector<int> freq(26,0);
-//             for(int j=i;j<n;j++){
-//                 freq[s[j]-'a']++;
-//                 unordered_set<int>st;
-//                 for(int k=0;k<26;k++){
-//                     if(freq[k]>0)
-//                         st.insert(freq[k]);
-//                 }
-//                 if(st.size()==1)
-//                     ans=max(ans,j-i+1);
-//             }
-//         }
-//         return ans;
-        
-//     }
-// };
 class Solution {
 public:
     int longestBalanced(string s) {
         int n = s.size();
+        vector<int> cnt(26, 0);
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            vector<int> freq(26, 0);
-            int distinct = 0;
-            int maxFreq = 0;
-
-            for (int j = i; j < n; j++) {
-                int idx = s[j] - 'a';
-
-                if (freq[idx] == 0)
-                    distinct++;
-
-                freq[idx]++;
-                maxFreq = max(maxFreq, freq[idx]);
-
-                if (maxFreq * distinct == j - i + 1)
+        for (int i = 0; i < n; ++i) {
+            fill(cnt.begin(), cnt.end(), 0);
+            int mx = 0, v = 0;
+            for (int j = i; j < n; ++j) {
+                int c = s[j] - 'a';
+                if (++cnt[c] == 1) {
+                    ++v;
+                }
+                mx = max(mx, cnt[c]);
+                if (mx * v == j - i + 1) {
                     ans = max(ans, j - i + 1);
+                }
             }
         }
         return ans;
-    }
-};
+    }};
