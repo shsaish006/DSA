fc97e0c909072ceead4458c1de0207263a2a167b
Shivam Kumar Singh <shsaish006@gmail.com>
2026-02-28 12:33:55 +0530

    Time: 28 ms (71.14%) | Memory: 7.9 MB (99.33%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -3,9 +3,15 @@ public:
     int concatenatedBinary(int n) {
         const int mod = 1e9 + 7;
         long long ans = 0;
-        for (int i = 1; i <= n; ++i) {
-            ans = (ans << (32 - __builtin_clz(i)) | i) % mod;
+        int len = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                len++;
+            }
+            ans = ((ans << len) + i) % mod;
         }
+        
         return ans;
     }
 };
