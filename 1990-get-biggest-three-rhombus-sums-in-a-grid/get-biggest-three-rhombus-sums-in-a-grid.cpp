95acd3d17efca8a3779837095a8cf10deaadc464
Shivam Kumar Singh <shsaish006@gmail.com>
2026-03-16 22:42:25 +0530

    Time: 156 ms (41.38%) | Memory: 65.1 MB (23.45%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    vector<int> getBiggestThree(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> s1(m + 1, vector<int>(n + 2));
-        vector<vector<int>> s2(m + 1, vector<int>(n + 2));
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                s1[i][j] = s1[i - 1][j - 1] + grid[i - 1][j - 1];
-                s2[i][j] = s2[i - 1][j + 1] + grid[i - 1][j - 1];
+    vector<int> getBiggestThree(vector<vector<int>>& nums) {
+        int a=nums.size(),b=nums[0].size();
+        set<int> s;
+        
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++){
+                
+                s.insert(nums[i][j]);
+                if(s.size()>3) s.erase(s.begin());
+                
+                for(int k=1;;k++){
+                    
+                    if(i-k<0||i+k>=a||j-k<0||j+k>=b) break;
+                    
+                    int curr=0;
+                    
+                    for(int d=0;d<k;d++) curr+=nums[i-k+d][j+d];
+                    for(int d=0;d<k;d++) curr+=nums[i+d][j+k-d];
+                    for(int d=0;d<k;d++) curr+=nums[i+k-d][j-d];
+                    for(int d=0;d<k;d++) curr+=nums[i-d][j-k+d];
+                    
+                    s.insert(curr);
+                    if(s.size()>3) s.erase(s.begin());
+                }
             }
         }
-        set<int> ss;
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                int l = min({i - 1, m - i, j - 1, n - j});
-                ss.insert(grid[i - 1][j - 1]);
-                for (int k = 1; k <= l; ++k) {
-                    int a = s1[i + k][j] - s1[i][j - k];
-                    int b = s1[i][j + k] - s1[i - k][j];
-                    int c = s2[i][j - k] - s2[i - k][j];
-                    int d = s2[i + k][j] - s2[i][j + k];
-                    ss.insert(a + b + c + d - grid[i + k - 1][j - 1] + grid[i - k - 1][j - 1]);
-                }
-                while (ss.size() > 3) {
-                    ss.erase(ss.begin());
-                }
-            }
-        }
-        return vector<int>(ss.rbegin(), ss.rend());
+        
+        vector<int> vals;
+        for(auto i=s.rbegin();i!=s.rend();i++) vals.push_back(*i);
+        return vals;
     }};
