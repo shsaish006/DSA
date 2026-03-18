b335b60f209dde727a3a95dfd0d37c73b19c0b3f
Shivam Kumar Singh <shsaish006@gmail.com>
2026-03-18 23:17:01 +0530

    Time: 24 ms (18.54%) | Memory: 101.1 MB (45.51%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int countSubmatrices(vector<vector<int>>& grid, int k) {
-        int m = grid.size(), n = grid[0].size();
-        int s[m + 1][n + 1];
-        memset(s, 0, sizeof(s));
-        int ans = 0;
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + grid[i - 1][j - 1];
-                if (s[i][j] <= k) {
-                    ++ans;
-                }
+    int countSubmatrices(vector<vector<int>>& nums, int k) {
+        int m = nums.size(), n = nums[0].size();
+        vector<vector<int>> s(m + 1, vector<int>(n + 1, 0));
+        int cnt = 0;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                s[i][j] = s[i-1][j] + s[i][j-1] - s[i-1][j-1] + nums[i-1][j-1];
+                if (s[i][j] <= k) cnt++;
             }
         }
-        return ans;
+        return cnt;
     }
 };
