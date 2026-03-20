962a6f65ca8f6b2880f42f7bda44a075862c9441
Shivam Kumar Singh <shsaish006@gmail.com>
2026-03-20 22:01:04 +0530

    Time: 16 ms (64.44%) | Memory: 31.9 MB (81.48%) - LeetSync

--- 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -1,26 +1,54 @@
 class Solution {
 public:
     vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> ans(m - k + 1, vector<int>(n - k + 1, 0));
-        for (int i = 0; i <= m - k; ++i) {
-            for (int j = 0; j <= n - k; ++j) {
-                vector<int> nums;
-                for (int x = i; x < i + k; ++x) {
-                    for (int y = j; y < j + k; ++y) {
-                        nums.push_back(grid[x][y]);
+        int c=grid.size(),d=grid[0].size();
+        int e=c-k+1,f=d-k+1;
+        
+        vector<vector<int>>g(e,vector<int>(f,0));
+        for(int h=0;h<e;++h){
+            for(int i=0;i<f;++i){
+                vector<int>j;
+                for(int l=h;l<h+k;++l){
+                    for(int m=i;m<i+k;++m){
+                        j.push_back(grid[l][m]);
                     }
                 }
-                ranges::sort(nums);
-                int d = INT_MAX;
-                for (int t = 1; t < nums.size(); ++t) {
-                    if (nums[t] != nums[t - 1]) {
-                        d = min(d, abs(nums[t] - nums[t - 1]));
+                sort(j.begin(),j.end());
+                int n=INT_MAX;
+                for(int o=1;o<j.size();++o){
+                    if(j[o]!=j[o-1]){
+                        n=min(n,j[o]-j[o-1]);
                     }
                 }
-                ans[i][j] = (d == INT_MAX) ? 0 : d;
+                g[h][i]=(n==INT_MAX)?0:n;
             }
         }
-        return ans;
+        return g;
     }
 };
+
+//         for(int f=0;f<=c-k;++f){
+//             for(int g=0;g<=d-k;++g){
+//                 //vector<int>h(k*k);
+//                 vector<int>h;
+//                 //int i=0;
+//                 for(int j=f;j<f+k;++j){
+//                     for(int l=g;k<g+k;++l){
+//                         h.push_back(grid[j][l]);
+//                         //h[i++]=grid[j][l];
+//                     }
+//                 }
+//                 sort(h.begin(),h.end());
+//                 int sai=INT_MAX;
+//                 for(int m=1;m<h.size();++m){
+//                     int n=h[m]-h[m-1];
+//                     if(n!=0&&n<1) sai=n;
+//                 }
+//                 e[f][g]=(sai==INT_MAX)?0:1;
+//             }
+            
+//         }
+//         return e;
+        
+//     }
+// };
