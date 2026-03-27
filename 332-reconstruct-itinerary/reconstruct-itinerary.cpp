5a1b98af4349e63ea8ad3991d3c7eef5ebe0cb56
Shivam Kumar Singh <shsaish006@gmail.com>
2026-03-27 23:31:10 +0530

    Time: 8 ms (48.00%) | Memory: 21.2 MB (40.55%) - LeetSync

---
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    unordered_map<string, vector<string>>g;
+    vector<string> ans;
+    void dfs(string a){
+        while(g[a].size()){
+            string b=g[a].back();
+            g[a].pop_back();
+            dfs(b);
+        }
+        ans.push_back(a);
+    }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        sort(tickets.begin(),tickets.end());
+        for(int i=tickets.size()-1;i>=0;i--){
+            g[tickets[i][0]].push_back(tickets[i][1]);
+        }
+        dfs("JFK");
+        reverse(ans.begin(), ans.end());
+        return ans;
+        
+        
+    }
+};
