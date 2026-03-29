9d4631638a55090b7186115eb226eebf7a183201
Shivam Kumar Singh <shsaish006@gmail.com>
2026-03-29 22:13:19 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (14.84%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
-    bool canBeEqual(string s1, string s2) {
-        vector<vector<int>> cnt(2, vector<int>(26, 0));
-        for (int i = 0; i < s1.size(); ++i) {
-            ++cnt[i & 1][s1[i] - 'a'];
-            --cnt[i & 1][s2[i] - 'a'];
-        }
-        for (int i = 0; i < 26; ++i) {
-            if (cnt[0][i] || cnt[1][i]) {
-                return false;
-            }
-        }
-        return true;
+    bool canBeEqual(string a, string b) {
+        vector<vector<int>> c(2, vector<int>(26));
+        for (int i = 0; i < a.size(); i++)
+            c[i&1][a[i]-'a']++, c[i&1][b[i]-'a']--;
+        for (auto &d : c)
+            for (int v : d)
+                if (v) return 0;
+        return 1;
     }
 };
