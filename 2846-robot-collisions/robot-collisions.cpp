c98cf6a3a9ea59c4e830e1a47e1d04edb330da72
Shivam Kumar Singh <shsaish006@gmail.com>
2026-04-01 22:18:30 +0530

    Time: 55 ms (68.66%) | Memory: 197.3 MB (39.32%) - LeetSync

--- 2846-robot-collisions/robot-collisions.cpp
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -1,102 +1,48 @@
-// class Solution {
-// public:
-//     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-//         int n=positions.size();
-//         vector<pair<int,vector<int>>>v(n);
-//         for(int i=0;i<n;i++){
-//             v[i]={positions[i],{i,healths[i],directions[i]}};
-//         }
-//         sort(v.begin(),v.end());
-//         stack<pair<int,vector<int>>>st;
-//         vector<pair<int,vector<int>>>ans;
-//         for(int i=0;i<n;i++){
-//             if(v[i].second[2]=='R'){
-//                 st.push(v[i]);
-//             } else{
-//                 bool ok=true;
-//                 while(!st.empty()){
-//                     pair<int,vector<int>> tp=st.top();
-//                     st.pop();
-//                     if(tp.second[1]==v[i].second[1]){
-//                         ok=false;
-//                         break;
-//                     }else if(tp.second[1]>v[i].second[1]){
-//                         ok=false;
-//                         tp.second[1]--;
-//                         st.push(tp);
-//                         break;
-//                     }else{
-//                         v[i].second[1]--;
-//                         continue;
-//                     }
-//                 }
-//                 if(ok){
-//                     ans.push_back(v[i]);
-//                 }
-//             }
-
-//         }
-//         while(!st.empty()){
-//             ans.push_back(st.top());
-//             st.pop();
-//         }
-//         for(int i=0;i<ans.size();i++){
-//             pair<int,vector<int>>temp=ans[i];
-//             ans[i].first=temp.second[0];
-//             ans[i].second[0]=temp.first;
-//         }
-//         sort(ans.begin(),ans.end());
-//         vector<int>res;
-//         for(int i=0;i<ans.size();i++){
-//             res.push_back(ans[i].second[1]);
-//         }
-//         return res;
-        
-//     }
-// };
 class Solution {
 public:
-    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        int n = positions.size();
+    vector<int> survivedRobotsHealths(vector<int>& a, vector<int>& b, string s) {
+        int n = a.size();
         vector<int> idx(n);
-        iota(idx.begin(), idx.end(), 0);
+        for (int i = 0; i < n; i++) idx[i] = i;
 
-        sort(idx.begin(), idx.end(), [&](int a, int b) {
-            return positions[a] < positions[b];
+        sort(idx.begin(), idx.end(), [&](int i, int j) {
+            return a[i] < a[j];
         });
 
-        vector<int> stk;
+        vector<int> d;
+        vector<int> on(n, 1);
 
-        for (int i : idx) {
-            if (directions[i] == 'R') {
-                stk.push_back(i);
-                continue;
-            }
+        for (int i = 0; i < n; i++) {
+            int curr = idx[i];
 
-            while (!stk.empty() && healths[i] > 0) {
-                int j = stk.back();
+            if (s[curr] == 'R') {
+                d.push_back(curr);
+            } else {
+                while (!d.empty() && b[curr] > 0) {
+                    int val = d.back();
 
-                if (healths[j] > healths[i]) {
-                    healths[j]--;
-                    healths[i] = 0;
-                } else if (healths[j] < healths[i]) {
-                    healths[i]--;
-                    healths[j] = 0;
-                    stk.pop_back();
-                } else {
-                    healths[i] = healths[j] = 0;
-                    stk.pop_back();
-                    break;
+                    if (b[val] < b[curr]) {
+                        d.pop_back();
+                        b[curr]--;
+                        on[val] = 0;
+                    } else if (b[val] > b[curr]) {
+                        b[val]--;
+                        on[curr] = 0;
+                        b[curr] = 0;
+                    } else {
+                        d.pop_back();
+                        on[val] = 0;
+                        on[curr] = 0;
+                        b[val] = 0;
+                        b[curr] = 0;
+                    }
                 }
             }
         }
 
-        vector<int> ans;
-        for (int h : healths) {
-            if (h > 0) {
-                ans.push_back(h);
-            }
+        vector<int> vals;
+        for (int i = 0; i < n; i++) {
+            if (on[i] && b[i] > 0) vals.push_back(b[i]);
         }
-        return ans;
-    }
-};
+        return vals;
+    }};
