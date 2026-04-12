8dc4c9976356e73c3f5ef6b151b0c5244bc7fd7b
Shivam Kumar Singh <shsaish006@gmail.com>
2026-04-12 22:02:44 +0530

    Time: 3 ms (89.56%) | Memory: 8.3 MB (98.80%) - LeetSync

--- 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     int minimumDistance(string word) {
-        int n = word.size();
-        const int inf = 1 << 30;
-        vector<vector<vector<int>>> f(n, vector<vector<int>>(26, vector<int>(26, inf)));
-        for (int j = 0; j < 26; ++j) {
-            f[0][word[0] - 'A'][j] = 0;
-            f[0][j][word[0] - 'A'] = 0;
-        }
-        for (int i = 1; i < n; ++i) {
+        vector<int> dp(26, 0), nxt(26, 1e9);
+
+        for (int i = 1; i < word.size(); i++) {
             int a = word[i - 1] - 'A';
             int b = word[i] - 'A';
-            int d = dist(a, b);
-            for (int j = 0; j < 26; ++j) {
-                f[i][b][j] = min(f[i][b][j], f[i - 1][a][j] + d);
-                f[i][j][b] = min(f[i][j][b], f[i - 1][j][a] + d);
-                if (j == a) {
-                    for (int k = 0; k < 26; ++k) {
-                        int t = dist(k, b);
-                        f[i][b][j] = min(f[i][b][j], f[i - 1][k][a] + t);
-                        f[i][j][b] = min(f[i][j][b], f[i - 1][a][k] + t);
-                    }
-                }
+
+            fill(nxt.begin(), nxt.end(), 1e9);
+
+            for (int j = 0; j < 26; j++) {
+                nxt[j] = min(nxt[j], dp[j] + dist(a, b));
+                nxt[a] = min(nxt[a], dp[j] + dist(j, b));
             }
+
+            dp = nxt;
         }
-        int ans = inf;
-        for (int j = 0; j < 26; ++j) {
-            ans = min(ans, f[n - 1][word[n - 1] - 'A'][j]);
-            ans = min(ans, f[n - 1][j][word[n - 1] - 'A']);
-        }
+
+        int ans = 1e9;
+        for (int i = 0; i < 26; i++) ans = min(ans, dp[i]);
         return ans;
     }
 
     int dist(int a, int b) {
-        int x1 = a / 6, y1 = a % 6;
-        int x2 = b / 6, y2 = b % 6;
-        return abs(x1 - x2) + abs(y1 - y2);
+        return abs(a / 6 - b / 6) + abs(a % 6 - b % 6);
     }
 };
