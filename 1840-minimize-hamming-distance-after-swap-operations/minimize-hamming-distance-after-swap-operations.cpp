b2f4aca179e6a42a937aa6cdccfe2bd66e55de25
Shivam Kumar Singh <shsaish006@gmail.com>
2026-04-21 22:18:57 +0530

    Time: 120 ms (89.53%) | Memory: 133 MB (93.19%) - LeetSync

--- 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
+++ 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
@@ -1,26 +1,51 @@
 class Solution {
 public:
+    vector<int> a;
+
+    int f(int b) {
+        if (a[b] == b) return b;
+        return a[b] = f(a[b]);
+    }
+
     int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
         int n = source.size();
-        vector<int> p(n);
-        iota(p.begin(), p.end(), 0);
-        function<int(int)> find = [&](int x) {
-            return x == p[x] ? x : p[x] = find(p[x]);
-        };
-        for (auto& a : allowedSwaps) {
-            p[find(a[0])] = find(a[1]);
+        a.resize(n);
+        iota(a.begin(), a.end(), 0);
+
+        for (auto &b : allowedSwaps) {
+            int c = f(b[0]), d = f(b[1]);
+            if (c != d) a[c] = d;
         }
-        unordered_map<int, unordered_map<int, int>> cnt;
-        for (int i = 0; i < n; ++i) {
-            ++cnt[find(i)][source[i]];
-            
+
+        unordered_map<int, vector<int>> b, c;
+        for (int i = 0; i < n; i++) {
+            int d = f(i);
+            b[d].push_back(source[i]);
+            c[d].push_back(target[i]);
         }
+
         int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            if (--cnt[find(i)][target[i]] < 0) {
-                ++ans;
+        for (auto &d : b) {
+            auto &s = d.second;
+            auto &o = c[d.first];
+            sort(s.begin(), s.end());
+            sort(o.begin(), o.end());
+
+            int i = 0, j = 0, cnt = 0;
+            while (i < s.size() && j < o.size()) {
+                if (s[i] == o[j]) {
+                    cnt++;
+                    i++;
+                    j++;
+                } else if (s[i] < o[j]) {
+                    i++;
+                } else {
+                    j++;
+                }
             }
+            ans += (int)s.size() - cnt;
         }
+
         return ans;
     }
 };
