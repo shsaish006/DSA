94857b663ab53e92632819137aebcce79982420d
Shivam Kumar Singh <shsaish006@gmail.com>
2026-04-22 20:01:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (97.33%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+    vector<string> twoEditWords(vector<string>& nums, vector<string>& vals) {
         vector<string> ans;
-        for (auto& s : queries) {
-            for (auto& t : dictionary) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < vals.size(); j++) {
                 int cnt = 0;
-                for (int i = 0; i < s.size(); ++i) {
-                    cnt += s[i] != t[i];
+                for (int k = 0; k < nums[i].size(); k++) {
+                    if (nums[i][k] != vals[j][k]) cnt++;
+                    if (cnt > 2) break;
                 }
-                if (cnt < 3) {
-                    ans.emplace_back(s);
+                if (cnt <= 2) {
+                    ans.push_back(nums[i]);
                     break;
                 }
             }
         }
         return ans;
-    }};
+    }
+};
