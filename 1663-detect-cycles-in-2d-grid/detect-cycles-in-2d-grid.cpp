c94205c675b82aebbc158f7522f8f764ea7d2483
Shivam Kumar Singh <shsaish006@gmail.com>
2026-04-26 19:27:29 +0530

    Time: 250 ms (19.48%) | Memory: 371.2 MB (5.04%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -1,34 +1,43 @@
 class Solution {
 public:
-    bool containsCycle(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<bool>> vis(m, vector<bool>(n));
-        const vector<int> dirs = {-1, 0, 1, 0, -1};
+    int a, b;
+    vector<vector<int>> nums;
+    vector<int> vals = {-1, 0, 1, 0, -1};
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (!vis[i][j]) {
-                    queue<array<int, 4>> q;
-                    q.push({i, j, -1, -1});
-                    vis[i][j] = true;
+    bool containsCycle(vector<vector<char>>& s) {
+        a = s.size();
+        b = s[0].size();
+        nums.assign(a, vector<int>(b, 0));
 
-                    while (!q.empty()) {
-                        auto p = q.front();
-                        q.pop();
-                        int x = p[0], y = p[1], px = p[2], py = p[3];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                if (!nums[i][j]) {
+                    queue<pair<int, int>> o;
+                    queue<pair<int, int>> on;
 
-                        for (int k = 0; k < 4; ++k) {
-                            int nx = x + dirs[k], ny = y + dirs[k + 1];
-                            if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                                if (grid[nx][ny] != grid[x][y] || (nx == px && ny == py)) {
-                                    continue;
-                                }
-                                if (vis[nx][ny]) {
-                                    return true;
-                                }
-                                q.push({nx, ny, x, y});
-                                vis[nx][ny] = true;
-                            }
+                    o.push({i, j});
+                    on.push({-1, -1});
+                    nums[i][j] = 1;
+
+                    while (!o.empty()) {
+                        auto curr = o.front();
+                        auto temp = on.front();
+                        o.pop();
+                        on.pop();
+
+                        for (int k = 0; k < 4; k++) {
+                            int c = curr.first + vals[k];
+                            int d = curr.second + vals[k + 1];
+
+                            if (c < 0 || d < 0 || c >= a || d >= b) continue;
+                            if (s[c][d] != s[curr.first][curr.second]) continue;
+                            if (c == temp.first && d == temp.second) continue;
+
+                            if (nums[c][d]) return true;
+
+                            nums[c][d] = 1;
+                            o.push({c, d});
+                            on.push(curr);
                         }
                     }
                 }
