1011ef3ef9b78e9ce84dadbf28dfa56c9fa15367
Shivam Kumar Singh <shsaish006@gmail.com>
2026-05-04 23:20:33 +0530

    Time: 91 ms (55.98%) | Memory: 27 MB (55.32%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int> vec;
-        int sum=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i;j<nums.size();j++){
-                sum=0;
-                for(int k=i;k<=j;k++) sum+=nums[k];
-                vec.push_back(sum);
+        vector<int> a;
+        for(int i=0;i<n;i++){
+            int s=0;
+            for(int j=i;j<n;j++){
+                s+=nums[j];
+                a.push_back(s);
             }
         }
-        sort(vec.begin(),vec.end());
-        sum=0;
-        int lim=1e9+7;
-        for(int i=left-1;i<right;i++) sum=(sum%lim)+vec[i];
-        return sum;
-
-        
-    }
+        sort(a.begin(),a.end());
+        long long d=0;
+        for(int i=left-1;i<right;i++)
+        d=(d+a[i])%(int)(1e9+7);
+        return d;
+        }
 };
