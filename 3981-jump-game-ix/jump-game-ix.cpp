b5cdf206685e303ea05267fa126af5f49d384836
Shivam Kumar Singh <shsaish006@gmail.com>
2026-05-07 13:13:29 +0530

    Time: 15 ms (47.34%) | Memory: 219.6 MB (79.26%) - LeetSync

--- 3981-jump-game-ix/jump-game-ix.cpp
+++ 3981-jump-game-ix/jump-game-ix.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     vector<int> maxValue(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n);
-        vector<int> preMax(n, nums[0]);
-        for (int i = 1; i < n; ++i) {
-            preMax[i] = max(preMax[i - 1], nums[i]);
+        int a = nums.size();
+        vector<int> vals(a), nums2(a);
+        nums2[0] = nums[0];
+        for (int i = 1; i < a; i++) {
+            nums2[i] = max(nums2[i - 1], nums[i]);
         }
-        int sufMin = 1 << 30;
-        for (int i = n - 1; i >= 0; --i) {
-            ans[i] = preMax[i] > sufMin ? ans[i + 1] : preMax[i];
-            sufMin = min(sufMin, nums[i]);
+        int b = nums[a - 1];
+        vals[a - 1] = nums2[a - 1];
+        for (int i = a - 2; i >= 0; i--) {
+            if (nums2[i] > b) vals[i] = vals[i + 1];
+            else vals[i] = nums2[i];
+
+            b = min(b, nums[i]);
         }
-        return ans;
+        return vals;
     }
 };
