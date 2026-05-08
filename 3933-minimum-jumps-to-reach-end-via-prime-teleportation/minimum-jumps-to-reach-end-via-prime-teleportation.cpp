3f18d0596378a78f232398eb306345cbd981721b
Shivam Kumar Singh <shsaish006@gmail.com>
2026-05-08 23:08:56 +0530

    Time: 435 ms (84.13%) | Memory: 334.4 MB (45.02%) - LeetSync

--- 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.cpp
+++ 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -1,56 +1,72 @@
-const int mx = 1e6 + 1;
-vector<int> factors[mx];
-
-int init = [] {
-    for (int i = 2; i < mx; ++i) {
-        if (factors[i].empty()) {
-            for (int j = i; j < mx; j += i) {
-                factors[j].push_back(i);
-            }
-        }
-    }
-    return 0;
-}();
-
 class Solution {
 public:
     int minJumps(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int, vector<int>> g;
-        for (int i = 0; i < n; i++) {
-            int x = nums[i];
-            for (int p : factors[x]) {
-                g[p].push_back(i);
-            }
-        }
-        int ans = 0;
-        vector<bool> vis(n, false);
-        vis[0] = true;
-        queue<int> q;
-        q.push(0);
-        while (true) {
-            queue<int> nq;
-            while (!q.empty()) {
-                int i = q.front();
-                q.pop();
-                if (i == n - 1) {
-                    return ans;
-                }
-                vector<int> idx = g[nums[i]];
-                idx.push_back(i + 1);
-                if (i > 0) {
-                    idx.push_back(i - 1);
-                }
-                for (int j : idx) {
-                    if (!vis[j]) {
-                        vis[j] = true;
-                        nq.push(j);
+        int a = nums.size();
+        int b = *max_element(nums.begin(), nums.end());
+
+        vector<int> vals(b + 1);
+        for (int i = 2; i <= b; i++) {
+            if (!vals[i]) {
+                vals[i] = i;
+                if ((long long)i * i <= b) {
+                    for (long long j = 1LL * i * i; j <= b; j += i) {
+                        if (!vals[j]) vals[j] = i;
                     }
                 }
-                g[nums[i]].clear();
             }
-            q = nq;
-            ans++;
         }
+
+        unordered_map<int, vector<int>> idx;
+        for (int i = 0; i < a; i++) {
+            int c = nums[i];
+            while (c > 1) {
+                int d = vals[c];
+                idx[d].push_back(i);
+                while (c % d == 0) c /= d;
+            }
+        }
+
+        queue<int> q;
+        vector<int> vis(a);
+        q.push(0);
+        vis[0] = 1;
+
+        int cnt = 0;
+
+        while (!q.empty()) {
+            int s = q.size();
+
+            while (s--) {
+                int i = q.front();
+                q.pop();
+
+                if (i == a - 1) return cnt;
+
+                if (i + 1 < a && !vis[i + 1]) {
+                    vis[i + 1] = 1;
+                    q.push(i + 1);
+                }
+
+                if (i - 1 >= 0 && !vis[i - 1]) {
+                    vis[i - 1] = 1;
+                    q.push(i - 1);
+                }
+
+                int c = nums[i];
+                if (c > 1 && vals[c] == c) {
+                    for (int j : idx[c]) {
+                        if (!vis[j]) {
+                            vis[j] = 1;
+                            q.push(j);
+                        }
+                    }
+                    idx[c].clear();
+                }
+            }
+
+            cnt++;
+        }
+
+        return -1;
     }
 };
