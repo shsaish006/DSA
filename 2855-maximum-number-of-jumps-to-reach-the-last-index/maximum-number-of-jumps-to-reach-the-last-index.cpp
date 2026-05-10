b486e38ed7d9d1ddd2f72f3e011fb185e00f1c55
Shivam Kumar Singh <shsaish006@gmail.com>
2026-05-10 22:49:41 +0530

    Time: 23 ms (43.48%) | Memory: 68.6 MB (40.06%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int maximumJumps(vector<int>& nums, int target) {
-        int n = nums.size();
-        int f[n];
-        memset(f, -1, sizeof(f));
-        auto dfs = [&](this auto&& dfs, int i) -> int {
-            if (i == n - 1) {
-                return 0;
-            }
-            if (f[i] != -1) {
-                return f[i];
-            }
-            f[i] = -(1 << 30);
-            for (int j = i + 1; j < n; ++j) {
-                if (abs(nums[i] - nums[j]) <= target) {
-                    f[i] = max(f[i], 1 + dfs(j));
-                }
-            }
-            return f[i];
-        };
-        int ans = dfs(0);
-        return ans < 0 ? -1 : ans; }};
+        int a=nums.size();
+        vector<int> vals(a,-1e9);
+        vals[0]=0;
+
+        for(int i=0;i<a;i++)
+            for(int j=i+1;j<a;j++)
+                if(abs(nums[i]-nums[j])<=target)
+                    vals[j]=max(vals[j],vals[i]+1);
+
+        return vals.back()<0?-1:vals.back();
+    }
+};
