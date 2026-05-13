cab544d648dca3c52b7c3aaaa1086e163cd21998
Shivam Kumar Singh <shsaish006@gmail.com>
2026-05-13 20:34:06 +0530

    Time: 0 ms (100.00%) | Memory: 93.4 MB (83.19%) - LeetSync

--- 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums, int limit) {
-        int n = nums.size();
-        int d[limit * 2 + 2];
-        memset(d, 0, sizeof(d));
-        for (int i = 0; i < n / 2; ++i) {
-            int x = nums[i], y = nums[n - i - 1];
-            if (x > y) {
-                swap(x, y);
-            }
-            d[2] += 2;
-            d[x + 1] -= 2;
-            d[x + 1] += 1;
-            d[x + y] -= 1;
-            d[x + y + 1] += 1;
-            d[y + limit + 1] -= 1;
-            d[y + limit + 1] += 2;
+        int a = nums.size();
+        vector<int> vals(2 * limit + 2, 0);
+        for (int idx = 0; idx < a / 2; idx++) {
+            int b = nums[idx];
+            int c = nums[a - idx - 1];
+            int d = min(b, c);
+            int s = max(b, c);
+            vals[2] += 2;
+            vals[d + 1] -= 1;
+            vals[b + c] -= 1;
+            vals[b + c + 1] += 1;
+            vals[s + limit + 1] += 1;
         }
-        int ans = n;
-        for (int i = 2, s = 0; i <= limit * 2; ++i) {
-            s += d[i];
-            ans = min(ans, s);
+
+        int ans = a;
+        int curr = 0;
+        for (int idx = 2; idx <= 2 * limit; idx++) {
+            curr += vals[idx];
+            ans = min(ans, curr);
         }
+
         return ans;
     }
 };
