57bf5240ebba2e2816a0fcfe6ac4d87f616760c4
Shivam Kumar Singh <shsaish006@gmail.com>
2026-05-21 10:24:49 +0530

    Time: 199 ms (49.83%) | Memory: 135.4 MB (83.17%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<int> s;
-        for (int x : arr1) {
-            for (; x; x /= 10) {
-                s.insert(x);
-            }
+    // int dp(string &a, string &b){
+    //     int n=min(a.size(), b.size()), cnt=0;
+    //     for(int i=0;i<n && a[i] == b[i]; i++) cnt++;
+    //     return cnt;
+    // }
+    int longestCommonPrefix(vector<int>& a, vector<int>& b) {
+        unordered_set< int> s;
+        int ans=0;
+        for(int x:a) while(x) s.insert(x), x/=10;
+        for(int x:b) while(x) {
+            if(s.count(x))
+            ans=max(ans,(int)to_string(x).size());
+        x/=10;
         }
-        int mx = 0; for (int x : arr2) {
-     for (; x; x /= 10) {
-                if (s.count(x)) {
-                    mx = max(mx, x);
-                    break;      }} }
-        return mx > 0 ? (int) log10(mx) + 1 : 0;
+        // sort(a.begin(), a.end());
+        // sort(b.begin(), b.end());
+        // int ans=0;
+        // for(int i=0;i<a.size(); i++){
+        //     string s=to_string (a[i]);
+        //     for(int j=0;j<b.size(); j++){
+        //         string t=to_string(b[j]);
+        //     int cnt=0, n=min(s.size(), t.size());
+        //     while(cnt< n && s[cnt]== t[cnt])
+        //     cnt++;
+        //     ans=max(ans,cnt);
+                // ans=max(ans,dp(s,t));
+        //     }
+        // }
+        return ans;
+        
     }
 };
+
+
