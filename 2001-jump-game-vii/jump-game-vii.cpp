fd34fe95fe097ee5bfa10dcc2bdbd55f5485432e
Shivam Kumar Singh <shsaish006@gmail.com>
2026-05-25 22:25:46 +0530

    Time: 18 ms (19.79%) | Memory: 38.4 MB (5.94%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.cpp
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     bool canReach(string s, int minJump, int maxJump) {
-        int n = s.size();
-        vector<int> pre(n + 1);
-        pre[1] = 1;
-        vector<bool> f(n);
-        f[0] = true;
-        for (int i = 1; i < n; ++i) {
-            if (s[i] == '0') {
-                int l = max(0, i - maxJump);
-                int r = i - minJump;
-                f[i] = l <= r && pre[r + 1] - pre[l];
-            }
-            pre[i + 1] = pre[i] + f[i];
+        
+        int n=s.size();
+        vector<int>pre(n+1),dp(n);
+        dp[0]=pre[1]=1;
+        for(int i=1;i<n;i++){
+            int l=max(0,i-maxJump),r=i-minJump;
+            if(s[i]=='0' && l<=r && pre[r+1]-pre[l])
+                dp[i]=1;
+            pre[i+1]=pre[i]+dp[i];
         }
-        return f[n - 1];
+        return dp[n-1];
     }
 };
