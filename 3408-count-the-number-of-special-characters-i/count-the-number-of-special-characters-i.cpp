64364affa12e1b5bb563e38df3b03965bd522c68
Shivam Kumar Singh <shsaish006@gmail.com>
2026-05-26 19:22:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (96.06%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        vector<bool> s('z' + 1);
-        for (char& c : word) {
-            s[c] = true;
-        }
-        int ans = 0;
-        for (int i = 0; i < 26; ++i) {
-            ans += s['a' + i] && s['A' + i];
-        }
+        int ans=0; for(char c='a';c<='z';c++) 
+        ans+=word.find(c)!=string::npos&&word.find(c-32)!=string::npos;
         return ans;
     }
 };
