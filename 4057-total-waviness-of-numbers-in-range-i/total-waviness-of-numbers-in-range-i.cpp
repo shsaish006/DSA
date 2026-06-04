74e0191c4d92d90780d0aad0ce1b57b3d309d7fc
Shivam Kumar Singh <shsaish006@gmail.com>
2026-06-04 23:27:13 +0530

    Time: 49 ms (50.93%) | Memory: 10.3 MB (33.96%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -1,29 +1,44 @@
 class Solution {
-public:
-    int totalWaviness(int num1, int num2) {
-        int ans = 0;
-        for (int x = num1; x <= num2; x++) {
-            ans += f(x);
-        }
-        return ans;
-    }
-
-    int f(int x) {
-        int nums[20], m = 0;
-        while (x > 0) {
-            nums[m++] = x % 10;
-            x /= 10;
-        }
-        if (m < 3) {
-            return 0;
-        }
-        int s = 0;
-        for (int i = 1; i < m - 1; i++) {
-            if ((nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) || (nums[i] < nums[i - 1] && nums[i] < nums[i + 1])) {
-                s++;
+    long long dp[20][11][11][2][20];
+    string s;
+    pair<long long,long long> dfs(int i, int p1, int p2, int st, int tight) {
+        if (i == s.size()) return {1, 0};
+        if (!tight && dp[i][p1][p2][st][0] != -1)
+            return {dp[i][p1][p2][st][0], dp[i][p1][p2][st][1]};
+            int lim = tight ? s[i] - '0' : 9;
+        long long ways = 0, sum = 0;
+        for (int d = 0; d <= lim; d++) {
+            int nt = tight && (d == lim);
+            if (!st && d == 0) {
+                auto [w, v] = dfs(i + 1, 10, 10, 0, nt);
+                ways += w;
+                sum += v;
+            } else {
+                int add = 0;
+                if (st && p2 != 10) {
+                    if ((p1 > p2 && p1 > d) || (p1 < p2 && p1 < d))
+                        add = 1;
+                }
+                auto [w, v] = dfs(i + 1, d, p1, 1, nt);
+                ways += w;
+                sum += v + 1LL * add * w;
             }
         }
-        
-        return s;
+        if (!tight) {
+            dp[i][p1][p2][st][0] = ways;
+            dp[i][p1][p2][st][1] = sum;
+        }
+
+        return {ways, sum};
+    }
+    long long solve(long long x) {
+        if (x < 0) return 0;
+        s = to_string(x);
+        memset(dp, -1, sizeof(dp));
+        return dfs(0, 10, 10, 0, 1).second;
+    }
+public:
+    int totalWaviness(int num1, int num2) {
+        return solve(num2) - solve(num1 - 1);
     }
 };
