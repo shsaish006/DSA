f90c2771b271a97e03b035c39145c47a78e74410
Shivam Kumar Singh <shsaish006@gmail.com>
2026-06-16 20:27:12 +0530

    Time: 4 ms (63.64%) | Memory: 52.4 MB (5.30%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     string processStr(string s) {
-        string result;
-        for (char c : s) {
-            if (isalpha(c)) {
-                result += c;
-            } else if (c == '*') {
-                if (!result.empty()) {
-                    result.pop_back();
-                }
-            } else if (c == '#') {
-                result += result;
-            } else if (c == '%') {
-                ranges::reverse(result);
-            }
+        int n=s.size();
+        vector<string> dp(n+1);
+        for(int i=0;i<n;i++){
+            dp[i+1]=dp[i];
+            char c=s[i];
+            if(isalpha(c)) dp[i+1]+=c;
+            else if(c=='*' && !dp[i+1].empty()) dp[i+1].pop_back();
+            else if(c=='#') dp[i+1]+=dp[i+1];
+            else if(c=='%') reverse(dp[i+1].begin(),dp[i+1].end());
         }
-        return result;
+
+        return dp[n];
     }
 };
