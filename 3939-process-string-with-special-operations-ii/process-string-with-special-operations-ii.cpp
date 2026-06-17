c597edfbcd52cd69a086ecf5c5206bdaed9b3ebf
Shivam Kumar Singh <shsaish006@gmail.com>
2026-06-17 23:07:02 +0530

    Time: 16 ms (95.40%) | Memory: 24.1 MB (28.74%) - LeetSync

--- 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
@@ -2,35 +2,22 @@ class Solution {
 public:
     char processStr(string s, long long k) {
         long long m = 0;
-        for (char c : s) {
-            if (c == '*') {
-                m = max(0LL, m - 1);
-            } else if (c == '#') {
-                m <<= 1;
-            } else if (c != '%') {
-                m += 1;
-            }
-        }
-        if (k >= m) {
-            return '.';
-        }
-        for (int i = s.length() - 1;; i--) {
+        for (char c : s)
+            m += (c >= 'a' && c <= 'z'), m -= (c == '*') && (m > 0), m *= (c == '#') ? 2 : 1;
+        if (k >= m) return '.';
+        for (int i = (int)s.size() - 1; i >= 0; i--) {
             char c = s[i];
-            if (c == '*') {
-                m += 1;
-            } else if (c == '#') {
-                m /= 2;
-                if (k >= m) {
-                    k -= m;
-                }
-            } else if (c == '%') {
-                k = m - 1 - k;
-            } else {
-                m -= 1;
-                if (k == m) {
-                    return c;
-                }
+            m += (c == '*');
+            if (c == '#') {
+                long long half = m / 2;
+                if (k >= half) k -= half;
+                m = half;
+            }
+            if (c == '%') k = m - 1 - k;
+            if (c >= 'a' && c <= 'z') {
+                if (--m == k) return c;
             }
         }
+        return '.';
     }
 };
