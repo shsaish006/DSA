5190e022b2854e1e5f8679eb424e6f75b1070573
Shivam Kumar Singh <shsaish006@gmail.com>
2026-06-24 21:55:06 +0530

    Time: 886 ms (37.21%) | Memory: 73.9 MB (11.63%) - LeetSync

--- 3964-number-of-zigzag-arrays-ii/number-of-zigzag-arrays-ii.cpp
+++ 3964-number-of-zigzag-arrays-ii/number-of-zigzag-arrays-ii.cpp
@@ -1,66 +1,60 @@
 class Solution {
-private:
-    static constexpr long long MOD = 1'000'000'007;
-    using Matrix = vector<vector<long long>>;
+    static const int mod = 1e9 + 7;
 
-    Matrix mul(const Matrix& a, const Matrix& b) {
+    using M = vector<vector<long long>>;
+
+    M mul(M &a, M &b) {
         int n = a.size();
-        int m = b[0].size();
-        Matrix res(n, vector<long long>(m, 0));
+        M c(n, vector<long long>(n));
 
-        for (int i = 0; i < n; i++) {
-            for (int k = 0; k < a[0].size(); k++) {
-                long long r = a[i][k];
-                if (r == 0) {
-                    continue;
-                }
-                for (int j = 0; j < m; j++) {
-                    res[i][j] = (res[i][j] + r * b[k][j]) % MOD;
-                }
-            }
-        }
-        return res;
+        for (int i = 0; i < n; i++)
+            for (int k = 0; k < n; k++)
+                if (a[i][k])
+                    for (int j = 0; j < n; j++)
+                        c[i][j] = (c[i][j] + a[i][k] * b[k][j]) % mod;
+
+        return c;
     }
 
-    Matrix powMul(Matrix base, long long exp, Matrix res) {
-        while (exp > 0) {
-            if (exp & 1) {
-                res = mul(res, base);
-            }
-            base = mul(base, base);
-            exp >>= 1;
+    M power(M a, long long p) {
+        int n = a.size();
+        M r(n, vector<long long>(n));
+
+        for (int i = 0; i < n; i++) r[i][i] = 1;
+
+        while (p) {
+            if (p & 1) r = mul(r, a);
+            a = mul(a, a);
+            p >>= 1;
         }
-        return res;
+
+        return r;
     }
 
 public:
     int zigZagArrays(int n, int l, int r) {
         int m = r - l + 1;
-        if (n == 1) {
-            return m;
-        }
 
-        int size = 2 * m;
-        Matrix u(size, vector<long long>(size, 0));
+        if (n == 1) return m;
+
+        int sz = 2 * m;
+        M t(sz, vector<long long>(sz));
 
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < i; j++) {
-                u[i][j + m] = 1;
-            }
-            for (int j = i + 1; j < m; j++) {
-                u[i + m][j] = 1;
-            }
+            for (int j = 0; j < i; j++)
+                t[i][j + m] = 1;
+
+            for (int j = i + 1; j < m; j++)
+                t[i + m][j] = 1;
         }
 
-        Matrix dp(1, vector<long long>(size, 1));
-
-        dp = powMul(std::move(u), n - 1, std::move(dp));
-
+        t = power(t, n - 1);
         long long ans = 0;
-        for (int i = 0; i < size; i++) {
-            ans = (ans + dp[0][i]) % MOD;
-        }
 
+        for (int i = 0; i < sz; i++)
+            for (int j = 0; j < sz; j++)
+                ans = (ans + t[i][j]) % mod;
         return ans;
     }
 };
+//  copy pasted i 
