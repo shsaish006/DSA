5744a4c9c2c7f74334d634eff94536588357ce74
Shivam Kumar Singh <shsaish006@gmail.com>
2026-06-27 10:21:01 +0530

    Time: 76 ms (72.57%) | Memory: 122.6 MB (47.43%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.cpp
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        unordered_map<long long, int> cnt;
-        for (int x : nums) {
-            ++cnt[x];
-        }
-        int ans = cnt[1] - (cnt[1] % 2 ^ 1);
-        cnt.erase(1);
-        for (auto [v, _] : cnt) {
-            int t = 0;
-            long long x = v;
-            while (cnt.count(x) && cnt[x] > 1) {
-                x = x * x;
-                t += 2;
+        unordered_map<long long,int> mp;
+        for(int x:nums) mp[x]++;
+        int ans=mp[1]-(mp[1]%2==0);
+        mp.erase(1);
+        for(unordered_map<long long,int>::iterator it=mp.begin();it!=mp.end();it++){
+            long long v=it->first;
+            int len=0;
+            while(mp.count(v)&&mp[v]>1){
+                len+=2;
+                v*=v;
             }
-            t += cnt.count(x) ? 1 : -1;
-            ans = max(ans, t);
+            len+=mp.count(v)?1:-1;
+            ans=max(ans,len);
         }
         return ans;
     }
