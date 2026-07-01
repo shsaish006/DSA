327b083c01863b64214402aaf63c9cfb57babc9c
Shivam Kumar Singh <shsaish006@gmail.com>
2026-07-01 13:38:55 +0530

    Time: 423 ms (61.23%) | Memory: 213.6 MB (28.35%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -1,78 +1,56 @@
-class UnionFind {
-public:
-    vector<int> p;
-    int n;
-
-    UnionFind(int _n)
-        : n(_n)
-        , p(_n) {
-        iota(p.begin(), p.end(), 0);
-    }
-
-    bool unite(int a, int b) {
-        int pa = find(a), pb = find(b);
-        if (pa == pb) return false;
-        p[pa] = pb;
-        --n;
-        return true;
-    }
-
-    int find(int x) {
-        if (p[x] != x) p[x] = find(p[x]);
-        return p[x];
-    }
-};
-
 class Solution {
 public:
-    int maximumSafenessFactor(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if (grid[0][0] || grid[n - 1][n - 1]) {
-            return 0;
-        }
-        queue<pair<int, int>> q;
-        int dist[n][n];
-        memset(dist, 0x3f, sizeof(dist));
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j]) {
-                    dist[i][j] = 0;
-                    q.emplace(i, j);
+    int maximumSafenessFactor(vector<vector<int>>& g) {
+        int n = g.size();
+        vector<vector<int>> d(n, vector<int>(n, 1e9));
+        queue<pair<int,int>> q;
+        int dir[5]={-1,0,1,0,-1};
+
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                if(g[i][j]){
+                    d[i][j]=0;
+                    q.push({i,j});
+                }
+
+        while(!q.empty()){
+            auto [x,y]=q.front(); q.pop();
+            for(int k=0;k<4;k++){
+                int nx=x+dir[k],ny=y+dir[k+1];
+                if(nx>=0&&ny>=0&&nx<n&&ny<n&&d[nx][ny]>d[x][y]+1){
+                    d[nx][ny]=d[x][y]+1;
+                    q.push({nx,ny});
                 }
             }
         }
-        int dirs[5] = {-1, 0, 1, 0, -1};
-        while (!q.empty()) {
-            auto [i, j] = q.front();
-            q.pop();
-            for (int k = 0; k < 4; ++k) {
-                int x = i + dirs[k], y = j + dirs[k + 1];
-                if (x >= 0 && x < n && y >= 0 && y < n && dist[x][y] == 0x3f3f3f3f) {
-                    dist[x][y] = dist[i][j] + 1;
-                    q.emplace(x, y);
+
+        int l=0,r=2*n;
+        while(l<r){
+            int m=(l+r+1)/2;
+            if(d[0][0]<m||d[n-1][n-1]<m){
+                r=m-1;
+                continue;
+            }
+
+            vector<vector<int>> vis(n,vector<int>(n));
+            queue<pair<int,int>> qq;
+            qq.push({0,0});
+            vis[0][0]=1;
+
+            while(!qq.empty()){
+                auto [x,y]=qq.front(); qq.pop();
+                for(int k=0;k<4;k++){
+                    int nx=x+dir[k],ny=y+dir[k+1];
+                    if(nx>=0&&ny>=0&&nx<n&&ny<n&&!vis[nx][ny]&&d[nx][ny]>=m){
+                        vis[nx][ny]=1;
+                        qq.push({nx,ny});
+                    }
                 }
             }
+
+            if(vis[n-1][n-1]) l=m;
+            else r=m-1;
         }
-        vector<tuple<int, int, int>> t;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                t.emplace_back(dist[i][j], i, j);
-            }
-        }
-        sort(t.begin(), t.end());
-        reverse(t.begin(), t.end());
-        UnionFind uf(n * n);
-        for (auto [d, i, j] : t) {
-            for (int k = 0; k < 4; ++k) {
-                int x = i + dirs[k], y = j + dirs[k + 1];
-                if (x >= 0 && x < n && y >= 0 && y < n && dist[x][y] >= d) {
-                    uf.unite(i * n + j, x * n + y);
-                }
-            }
-            if (uf.find(0) == uf.find(n * n - 1)) {
-                return d;
-            }
-        }
-        return 0;
+        return l;
     }
 };
