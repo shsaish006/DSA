c8f5ed74d177bd9c8693a5776c52a5378490fc17
Shivam Kumar Singh <shsaish006@gmail.com>
2026-07-05 17:09:42 +0530

    Time: 3 ms (97.91%) | Memory: 11.9 MB (49.13%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.cpp
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    vector<int> pathsWithMaxScore(vector<string>& board) {
-        int n = board.size();
-        const int mod = 1e9 + 7;
-        int f[n][n];
-        int g[n][n];
-        memset(f, -1, sizeof(f));
-        memset(g, 0, sizeof(g));
-        f[n - 1][n - 1] = 0;
-        g[n - 1][n - 1] = 1;
+    vector<int> pathsWithMaxScore(vector<string>& b) {
+        int n = b.size(), mod = 1e9 + 7;
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        vector<vector<int>> cnt(n + 1, vector<int>(n + 1));
 
-        auto update = [&](int i, int j, int x, int y) {
-            if (x >= n || y >= n || f[x][y] == -1 || board[i][j] == 'X' || board[i][j] == 'S') {
-                return;
-            }
-            if (f[x][y] > f[i][j]) {
-                f[i][j] = f[x][y];
-                g[i][j] = g[x][y];
-            } else if (f[x][y] == f[i][j]) {
-                g[i][j] = (g[i][j] + g[x][y]) % mod;
-            }
-        };
+        dp[n - 1][n - 1] = 0;
+        cnt[n - 1][n - 1] = 1;
 
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = n - 1; j >= 0; --j) {
-                update(i, j, i + 1, j);
-                update(i, j, i, j + 1);
-                update(i, j, i + 1, j + 1);
-                if (f[i][j] != -1) {
-                    if (board[i][j] >= '0' && board[i][j] <= '9') {
-                        f[i][j] += (board[i][j] - '0');
-                    }
-                }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (b[i][j] == 'X' || (i == n - 1 && j == n - 1)) continue;
+
+                int mx = max({dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1]});
+                if (mx == -1) continue;
+
+                dp[i][j] = mx + (isdigit(b[i][j]) ? b[i][j] - '0' : 0);
+
+                if (dp[i + 1][j] == mx) cnt[i][j] = (cnt[i][j] + cnt[i + 1][j]) % mod;
+                if (dp[i][j + 1] == mx) cnt[i][j] = (cnt[i][j] + cnt[i][j + 1]) % mod;
+                if (dp[i + 1][j + 1] == mx) cnt[i][j] = (cnt[i][j] + cnt[i + 1][j + 1]) % mod;
             }
         }
-        vector<int> ans(2);
-        if (f[0][0] != -1) {
-            ans[0] = f[0][0];
-            ans[1] = g[0][0];
-        }
-        return ans;
+
+        if (dp[0][0] == -1) return {0, 0};
+        return {dp[0][0], cnt[0][0]};
     }
 };
