e22c6f487f50803c0addc7f818308fb9e6c7a466
Shivam Kumar Singh <shsaish006@gmail.com>
2026-07-08 14:24:53 +0530

    Time: 38 ms (68.91%) | Memory: 152 MB (74.09%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
-        static const int MX = 100001;
-        static const int MOD = 1000000007;
-        static vector<long long> pow10 = [] {
-            vector<long long> p(MX);
-            p[0] = 1;
-            for (int i = 1; i < MX; i++) {
-                p[i] = p[i - 1] * 10 % MOD;
-            }
-            return p;
-        }();
-
-        int n = s.size();
-        vector<int> sumD(n + 1), cntN0(n + 1);
-        vector<long long> p(n + 1);
-
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& q) {
+        int n = s.size(), mod = 1e9 + 7;
+        vector<int> sm(n + 1), c(n + 1);
+        vector<long long> p(n + 1), pw(n + 1, 1);
         for (int i = 1; i <= n; i++) {
+            pw[i] = pw[i - 1] * 10 % mod;
             int d = s[i - 1] - '0';
-            sumD[i] = sumD[i - 1] + d;
-            cntN0[i] = cntN0[i - 1] + (d > 0);
-            p[i] = d ? (p[i - 1] * 10 + d) % MOD : p[i - 1];
+            sm[i] = sm[i - 1] + d;
+            c[i] = c[i - 1] + (d > 0);
+            p[i] = (p[i - 1] * (d ? 10 : 1) + d) % mod;
         }
 
         vector<int> ans;
-        ans.reserve(queries.size());
-        for (auto& q : queries) {
-            int l = q[0], r = q[1];
-            int n0 = cntN0[r + 1] - cntN0[l];
-            int sd = sumD[r + 1] - sumD[l];
-            long long x = (p[r + 1] - p[l] * pow10[n0] % MOD + MOD) % MOD;
-            ans.push_back(x * sd % MOD);
+        for (auto &i : q) {
+            int l = i[0], r = i[1];
+            ans.push_back((p[r + 1] - p[l] * pw[c[r + 1] - c[l]] % mod + mod) % mod *
+                          1LL * (sm[r + 1] - sm[l]) % mod);
         }
         return ans;
     }
