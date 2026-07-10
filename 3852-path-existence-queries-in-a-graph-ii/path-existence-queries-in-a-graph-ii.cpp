54f8d747d69b1c6368862ec689aa2c100083c15b
Shivam Kumar Singh <shsaish006@gmail.com>
2026-07-10 21:30:18 +0530

    Time: 376 ms (26.32%) | Memory: 394.2 MB (32.89%) - LeetSync

--- 3852-path-existence-queries-in-a-graph-ii/path-existence-queries-in-a-graph-ii.cpp
+++ 3852-path-existence-queries-in-a-graph-ii/path-existence-queries-in-a-graph-ii.cpp
@@ -1,29 +1,52 @@
 class Solution {
 public:
-    vector<int> pathExistenceQueries(int n, vector<int>& a, int x, vector<vector<int>>& q) {
-        vector<pair<int,int>> v;
-        for(int i=0;i<n;i++) v.push_back({a[i],i});
-        sort(v.begin(),v.end());
+    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<pair<int, int>> pairs;
+        for (int i = 0; i < n; i++) {
+            pairs.emplace_back(nums[i], i);
+        }
+        sort(pairs.begin(), pairs.end());
 
-        vector<vector<int>> f(n,vector<int>(20));
-        for(int i=n-1,j=n-1;i>=0;i--){
-            while(v[j].first-v[i].first>x) j--;
-            f[v[i].second][0]=v[j].second;
-            for(int k=1;k<20;k++) f[v[i].second][k]=f[f[v[i].second][k-1]][k-1];
+        int m = 20;
+        vector<vector<int>> f(n, vector<int>(m));
+        int r = n - 1;
+        for (int l = n - 1; l >= 0; l--) {
+            while (pairs[r].first - pairs[l].first > maxDiff) {
+                r--;
+            }
+            int i = pairs[l].second, j = pairs[r].second;
+            f[i][0] = j;
+            for (int k = 1; k < m; k++) {
+                f[i][k] = f[f[i][k - 1]][k - 1];
+            }
         }
 
         vector<int> ans;
-        for(auto &e:q){
-            int i=e[0],j=e[1];
-            if(a[i]>a[j]) swap(i,j);
-            if(i==j){ ans.push_back(0); continue; }
-            if(a[i]==a[j]){ ans.push_back(1); continue; }
-
-            int d=0;
-            for(int k=19;k>=0;k--)
-                if(a[f[i][k]]<a[j]) d|=1<<k,i=f[i][k];
-
-            ans.push_back(a[f[i][0]]<a[j]?-1:d+1);
+        for (auto& q : queries) {
+            int i = q[0], j = q[1];
+            if (nums[i] > nums[j]) {
+                swap(i, j);
+            }
+            if (i == j) {
+                ans.push_back(0);
+                continue;
+            }
+            if (nums[i] == nums[j]) {
+                ans.push_back(1);
+                continue;
+            }
+            int d = 0;
+            for (int k = m - 1; k >= 0; k--) {
+                if (nums[f[i][k]] < nums[j]) {
+                    d |= 1 << k;
+                    i = f[i][k];
+                }
+            }
+            if (nums[f[i][0]] < nums[j]) {
+                ans.push_back(-1);
+            } else {
+                ans.push_back(d + 1);
+            }
         }
         return ans;
     }
