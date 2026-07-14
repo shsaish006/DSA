bb1e2217e8ccfdb524b6f5b0fc24da8c48619b5f
Shivam Kumar Singh <shsaish006@gmail.com>
2026-07-14 15:48:32 +0530

    Time: 406 ms (73.91%) | Memory: 290.5 MB (45.22%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
+int mod=1e9+7;
+vector<vector<vector<int>>> dp;
+int dfs(int i, int a, int b, vector<int> &v){
+    if(i==v.size()) return a==b;
+    int &x = dp[i][a][b];
+    if(x!=-1) return x;
+    return x =((dfs(i+1,a,b,v)+dfs(i+1,gcd(a,v[i]), b, v))%mod+ dfs(i+1,a,gcd(b,v[i]),v))%mod;
+}
+
     int subsequencePairCount(vector<int>& nums) {
-        const int MOD = 1e9 + 7;
-        int n = nums.size(); int m = ranges::max(nums);
-        vector f(n, vector(m + 1, vector<int>(m + 1, -1)));
-        auto dfs = [&](this auto&& dfs, int i, int j, int k) -> int {
-            if (i < 0) {
-                return j == k;
-            } int& res = f[i][j][k];
-            if (res < 0) {
-                res = ((dfs(i - 1, j, k)
-                           + dfs(i - 1, gcd(nums[i], j), k))
-                              % MOD
-                          + dfs(i - 1, j, gcd(nums[i], k)))
-                    % MOD;
-            }
-            return res;
-        };
-        return (dfs(n - 1, 0, 0) - 1 + MOD) % MOD;
+        int m=*max_element(nums.begin(), nums.end());
+        dp.assign(nums.size(),vector<vector<int>>(m+1, vector<int> (m+1,-1)));
+        return (dfs(0,0,0,nums)-1+mod)%mod;
+        
     }
 };
