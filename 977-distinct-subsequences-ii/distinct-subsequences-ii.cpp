263bab73a038b5540b09d27fbf6c84bb1499d9b2
Shivam Kumar Singh <shsaish006@gmail.com>
2026-09-07 20:20:09 +0530

    Time: 4 ms (23.13%) | Memory: 8.8 MB (78.81%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        const int mod = 1e9 + 7;
+        int f[26]{};
+        for (char& c : s) {
+            int x = 1;
+            for (int v : f) {
+                x = (x + v) % mod;
+            }
+            f[c - 'a'] = x;
+        }int ans = 0;
+        for (int v : f) {
+            ans = (ans + v) % mod;
+        }
+        return ans;
+    }
+};
